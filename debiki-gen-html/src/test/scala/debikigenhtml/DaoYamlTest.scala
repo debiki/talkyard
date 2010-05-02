a0a4f9ec858121772d4a186a78eb27279d2cd24f
Kaj Magnus Lindberg <kaj.magnus.lindberg@gmail.com>
2010-05-02 22:34:11 +0200

    Add test posts to _debate.yaml.
    Also update test cases.

--- debiki-gen-html/src/test/scala/debikigenhtml/DaoYamlTest.scala
+++ debiki-gen-html/src/test/scala/debikigenhtml/DaoYamlTest.scala
@@ -28,7 +28,7 @@ class DaoYamlTest {
     val dao: Dao = new DaoYaml
     val debate = dao.getDebate(Paths.MmaDebate)
     assert(debate.id == "compactDebate")
-    assert(debate.postsById.size == 8)
+    assert(debate.postsById.size == 11)
   }
 
 }
