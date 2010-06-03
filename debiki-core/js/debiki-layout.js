2b73002e7805d8af1687c2eb11f88c751d94b359
Kaj Magnus Lindberg <kaj.magnus.lindberg@gmail.com>
2010-06-03 21:56:47 +0200

    Make a public DebikiLayout object.
    So layout functionality is accessible from other modules.

--- debiki-core/js/debiki-layout.js
+++ debiki-core/js/debiki-layout.js
@@ -2,19 +2,25 @@
 
 // This script layouts the discussion, perhaps
 // in a readable and concise manner.
+// It also creates a public DebikiLayout object with layout methods.
 
 jQuery.noConflict()(function($){
 
-var dummyScope;
+// Public layout class.
+DebikiLayout = {
 
-// Make root thread wide enough to contain all its child posts.
-dummyScope = function(){
-  var width = 200;
-  var root = $('.debiki > .dw-thread');
-  root.children('.dw-thread').each(function(){
-    width += $(this).outerWidth(true);
-  });
-  root.css('width', width);
-}();
+  // Makes the root thread wide enough to contain all its child posts.
+  resizeRootThread: function(){
+    var width = 200;
+    var root = $('.debiki > .dw-thread');
+    root.children('.dw-thread').each(function(){
+      width += $(this).outerWidth(true);
+    });
+    root.css('width', width);
+  }
+}
+
+// Layout discussion.
+DebikiLayout.resizeRootThread();
 
 });
