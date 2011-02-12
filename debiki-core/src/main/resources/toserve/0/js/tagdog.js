aead46512c3cc0224b99d3c8930824cd9e5e57b4
Kaj Magnus Lindberg <kaj.magnus.lindberg@gmail.com>
2011-02-12 08:10:03 +0100

    Add almost empty tagdog.js
    TagDog searches text inside html tags, without being so very confused by
    tags and attributes.

---
+++ debiki-core/src/main/resources/toserve/0/js/tagdog.js
@@ -0,0 +1,114 @@
+// vim: fdm=marker et ts=2 sw=2 tw=80 fo=tcqwn list
+
+// The function below, html.makeHtmlSanitizer, is a copy of
+// function html.makeHtmlSanitizer, from html-sanitizer.js,
+// from google-caja, SVN r4338, 2010-12-20
+//    http://google-caja.googlecode.com/svn/trunk/src/com/google/caja
+//
+// {{{ The license
+// Copyright (C) 2006 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// }}}
+//
+html.makeHtmlSanitizer = function (sanitizeAttributes) {
+  var stack;
+  var ignoring;
+  return html.makeSaxParser({
+        startDoc: function (_) {
+          stack = [];
+          ignoring = false;
+        },
+        startTag: function (tagName, attribs, out) {
+          if (ignoring) { return; }
+          if (!html4.ELEMENTS.hasOwnProperty(tagName)) { return; }
+          var eflags = html4.ELEMENTS[tagName];
+          if (eflags & html4.eflags.FOLDABLE) {
+            return;
+          } else if (eflags & html4.eflags.UNSAFE) {
+            ignoring = !(eflags & html4.eflags.EMPTY);
+            return;
+          }
+          attribs = sanitizeAttributes(tagName, attribs);
+          // TODO(mikesamuel): relying on sanitizeAttributes not to
+          // insert unsafe attribute names.
+          if (attribs) {
+            if (!(eflags & html4.eflags.EMPTY)) {
+              stack.push(tagName);
+            }
+
+            out.push('<', tagName);
+            for (var i = 0, n = attribs.length; i < n; i += 2) {
+              var attribName = attribs[i],
+                  value = attribs[i + 1];
+              if (value !== null && value !== void 0) {
+                out.push(' ', attribName, '="', html.escapeAttrib(value), '"');
+              }
+            }
+            out.push('>');
+          }
+        },
+        endTag: function (tagName, out) {
+          if (ignoring) {
+            ignoring = false;
+            return;
+          }
+          if (!html4.ELEMENTS.hasOwnProperty(tagName)) { return; }
+          var eflags = html4.ELEMENTS[tagName];
+          if (!(eflags & (html4.eflags.UNSAFE | html4.eflags.EMPTY
+                          | html4.eflags.FOLDABLE))) {
+            var index;
+            if (eflags & html4.eflags.OPTIONAL_ENDTAG) {
+              for (index = stack.length; --index >= 0;) {
+                var stackEl = stack[index];
+                if (stackEl === tagName) { break; }
+                if (!(html4.ELEMENTS[stackEl] & html4.eflags.OPTIONAL_ENDTAG)) {
+                  // Don't pop non optional end tags looking for a match.
+                  return;
+                }
+              }
+            } else {
+              for (index = stack.length; --index >= 0;) {
+                if (stack[index] === tagName) { break; }
+              }
+            }
+            if (index < 0) { return; }  // Not opened.
+            for (var i = stack.length; --i > index;) {
+              var stackEl = stack[i];
+              if (!(html4.ELEMENTS[stackEl] & html4.eflags.OPTIONAL_ENDTAG)) {
+                out.push('</', stackEl, '>');
+              }
+            }
+            stack.length = index;
+            out.push('</', tagName, '>');
+          }
+        },
+        pcdata: function (text, out) {
+          if (!ignoring) { out.push(text); }
+        },
+        rcdata: function (text, out) {
+          if (!ignoring) { out.push(text); }
+        },
+        cdata: function (text, out) {
+          if (!ignoring) { out.push(text); }
+        },
+        endDoc: function (out) {
+          for (var i = stack.length; --i >= 0;) {
+            out.push('</', stack[i], '>');
+          }
+          stack.length = 0;
+        }
+      });
+};
+
+
