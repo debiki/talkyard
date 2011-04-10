81cf7bafeae3ba4ec4db0461954a327f0185d394
Kaj Magnus Lindberg <kaj.magnus.lindberg@gmail.com>
2011-04-10 09:13:58 +0200

    JS OpenID Selector: Fix null access
    Fix a harmless `url.indexOf' where `url' is null. The bug is harmless;
    the form is submitted despite the error.

--- debiki-core/src/main/resources/toserve/0/lib/openid-selector/js/openid-jquery.js
+++ debiki-core/src/main/resources/toserve/0/lib/openid-selector/js/openid-jquery.js
@@ -117,7 +117,7 @@ openid = {
 			alert(openid.demo_text + "\r\n" + document.getElementById(openid.input_id).value);
 			return false;
 		}
-		if (url.indexOf("javascript:") == 0) {
+		if (url && url.indexOf("javascript:") == 0) {
 			url = url.substr("javascript:".length);
 			eval(url);
 			return false;
