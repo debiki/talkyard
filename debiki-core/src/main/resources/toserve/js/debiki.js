4fbddf9698e21e75f309e239e58c0c6986c32906
Kaj Magnus Lindberg <kaj.magnus.lindberg@gmail.com>
2011-04-12 08:59:49 +0200

    Submit OpenID login form in popup.

--- debiki-core/src/main/resources/toserve/js/debiki.js
+++ debiki-core/src/main/resources/toserve/js/debiki.js
@@ -1118,6 +1118,98 @@ function $showLoginDialog() {
   return false;  // skip default action
 }
 
+// Submits an OpenID login <form> in a popup. Dims the window and
+// listens for the popup to close.
+function submitLoginInPopup($openidLoginForm) {
+  // Based on popupManager.createPopupOpener, from popuplib.js,
+  // in this folder.
+
+  var width = 450;
+  var height = 500;
+  var coordinates = popupManager.getCenteredCoords(width, height);
+
+  // Here is described how to configure the popup window:
+  // http://svn.openid.net/repos/specifications/user_interface/1.0/trunk
+  //    /openid-user-interface-extension-1_0.html
+  var popupWindow = window.open('', 'LoginPopup',
+      'width='+ width +',height='+ height +
+      ',status=1,location=1,resizable=yes'+
+      ',left='+ coordinates[0] +',top='+ coordinates[1]);
+
+  // Check to perform at each execution of the timed loop. It also triggers
+  // the action that follows the closing of the popup
+  var waitCallback = window.setInterval(waitForPopupClose, 80);
+  function waitForPopupClose() {
+    if (popupWindow && !popupWindow.closed) return;
+    popupWindow = null;
+    var darkCover = window.document.getElementById(
+        window.popupManager.constants['darkCover']);
+    if (darkCover) {
+      darkCover.style.visibility = 'hidden';
+    }
+    if (Debiki.handleLoginResponse !== null) {
+      Debiki.handleLoginResponse();
+    }
+    if (waitCallback !== null) {
+      window.clearInterval(waitCallback);
+      waitCallback = null;
+    }
+  }
+
+  // This callback is called from the return_to page:
+  Debiki.handleLoginResponse = function(queryString) {
+    Debiki.handleLoginResponse = null;
+    // When I click "No thanks", Google says:
+    // "openid.mode=cancel&
+    //  openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0"
+    if (/openid\.mode=cancel/.test(queryString)) {
+      // Login failed: User clicked No Thanks in some login dialog.
+    } else if (!queryString) {
+      // Login failed: User closed popup window?
+    } else {
+      // Login OK
+      // {{{ The queryString is e.g. …
+      // openid.ns=http://specs.openid.net/auth/2.0
+      // openid.mode=id_res
+      // openid.op_endpoint=https://www.google.com/accounts/o8/ud
+      // openid.response_nonce=2011-04-10T20:14:19Zwq0i9rEOAN0QsA
+      // openid.return_to=http://10.42.43.10:8080/openid/response
+      // openid.assoc_handle=AOQobUdh75yilxlGb-KbwvcLIocAG...
+      // openid.signed=op_endpoint,claimed_id,identity,return_to,
+      //    response_nonce,assoc_handle,ns.ext1,ext1.mode,ext1.type.first,
+      //    ext1.value.first,ext1.type.email,ext1.value.email,
+      //    ext1.type.country,ext1.value.country
+      // openid.sig=jlCF7WrP99%2Be1Ee8eq1s03JUE0h4wILx37FHZkv/KlA=
+      // openid.identity=https://www.google.com/accounts/o8/id?id=AItOaw...
+      // openid.claimed_id=https://www.google.com/accounts/o8/id?id=AItO...
+      // openid.ns.ext1=http://openid.net/srv/ax/1.0
+      // openid.ext1.mode=fetch_response
+      // openid.ext1.type.first=http://axschema.org/namePerson/first
+      // openid.ext1.value.first=Kaj+Magnus
+      // openid.ext1.type.email=http://axschema.org/contact/email
+      // openid.ext1.value.email=someone@example.com
+      // openid.ext1.type.country=http://axschema.org/contact/country/home
+      // openid.ext1.value.country=SE
+      // }}}
+      // TODO the server should remember email, first-name & country.
+      // TODO javascript-update everything, say Welcome X and replace
+      // "Log in" buttons with info "Logged in as ..." ??
+
+      return;
+    }
+    // Handle failures: Close all login dialogs and show a modal failure
+    // info dialog.
+  }
+
+  // TODO dim the main win, open a modal dialog: "Waiting for you to log in",
+  // and a Cancel button, which closes the popup window.
+  // — Then the user can continue also if the popup gets lost (perhaps
+  // lots of windows open).
+
+  // Make the default submit action submit the login form in the popup window.
+  $openidLoginForm.attr('target', 'LoginPopup');
+}
+
 
 // ------- Rating
 
@@ -2027,6 +2119,7 @@ $(".debiki .dw-p").each($initPost);
 
 
 openid.img_path = '/classpath/0/lib/openid-selector/images/';
+openid.submitInPopup = submitLoginInPopup;
 // Keep default openid.cookie_expires, 1000 days; COULD remove cookie on logout?
 openid.init('openid_identifier');
 initLoginDialog();
