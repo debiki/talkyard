281230f5bac02eae5d07c1998c201ea3e4e15d56
Kaj Magnus Lindberg <kaj.magnus.lindberg@gmail.com>
2011-05-29 10:31:03 +0200

    Add SBT project files, and initial TCK version.

---
+++ src/main/scala/com/debiki/v0/tck/DaoTckTest.scala
@@ -0,0 +1,136 @@
+package com.debiki.v0.tck
+
+/**
+ * Copyright (c) 2011 Kaj Magnus Lindberg (born 1979)
+ * Created on 2011-05-29.
+ */
+
+import com.debiki.v0
+import com.debiki.v0.Prelude._
+import org.specs._
+import org.specs.specification.PendingUntilFixed
+import org.specs.specification.Context
+import java.{util => ju}
+import net.liftweb.common._
+
+/*
+======================================
+ Technology Compatibility Kit (TCK)
+======================================
+
+Test design, including tests of upgrades:
+
+For each released database structure version:
+  1. execute the test suite (for that old version)
+  2. upgrade to the most recent version
+  3. execute the test suite (for the most recent version)
+      revert the upgrade (e.g. revert to a restore point) so the schema
+      remains unchanged, although an upgrade was tested.
+
+The test suite for a certain version requires two schemas:
+  - One completely empty
+  - One with fairly much contents (for performance tests)
+
+Could test:
+   Many server instances (i.e. many DAOs) access the database at the
+   same time, only one DAO should do the upgrade.
+
+*/
+
+trait TestContext {
+  def dao: v0.Dao
+  def close() = dao.close()
+  def createRestorePoint(): Unit
+  def revertToRestorePoint(): Unit
+}
+
+object DaoTckTest {
+  sealed class What
+  case object EmptySchema extends What
+  case object EmptyTables extends What
+  case object TablesWithData extends What
+
+  type TestContextBuilder = Function2[What, String, TestContext]
+
+  val defaultTenantId = "default"
+}
+
+import DaoTckTest._
+
+
+abstract class DaoTckTest(builder: TestContextBuilder)
+    extends SpecificationWithJUnit {
+
+  "The Technology Compatibility Kit".isSpecifiedBy(
+      new DaoSpecEmptySchema(builder),
+      new DaoSpecV002(builder))
+}
+
+
+abstract class DaoSpec(builder: TestContextBuilder, defSchemaVersion: String)
+    extends SpecificationWithJUnit {
+
+  // "SUS" means Systems under specification, which is a
+  // "The system" should { ...examples... } block.
+  // See: <http://code.google.com/p/specs/wiki/DeclareSpecifications
+  //        #Systems_under_specification>
+  def setup(what: What, version: String = defSchemaVersion) = new Context {
+    beforeSus({
+      ctx = builder(what, version)
+    })
+  }
+
+  var ctx: TestContext = _  // the speccontext below closes it after each test
+  def dao = ctx.dao
+
+  // close the dao and any db connections after each tests.
+  // see: <http://code.google.com/p/specs/wiki/declarespecifications
+  //                #specification_context>
+  // (without `close()', the specs test framework says:
+  // "[error] could not run test com.debiki.v0.oracledaotcktest:
+  // org.specs.specification.pathexception: treepath(list(0, 0, 1))
+  // not found for <the test name>")
+  new SpecContext {
+    afterSus({
+      ctx.close()
+    })
+  }
+
+}
+
+
+class DaoSpecEmptySchema(b: TestContextBuilder) extends DaoSpec(b, "0") {
+  val schemaIsEmpty = setup(EmptySchema)
+
+  "A v0.DAO in a completely empty repo" when schemaIsEmpty should {
+    "consider the version being 0" >> {
+      dao.checkRepoVersion() must_== Full("0")
+    }
+    "be able to upgrade to 0.0.2" >> {
+      // dao.upgrade()  currently done automatically, but ought not to.
+      dao.checkRepoVersion() must_== Full("0.0.2")
+    }
+  }
+}
+
+
+class DaoSpecV002(b: TestContextBuilder) extends DaoSpec(b, "0.0.2") {
+  val tablesAreEmpty = setup(EmptyTables)
+
+  "A v0.DAO in an empty 0.0.2 repo" when tablesAreEmpty should {
+    "find version 0.0.2" >> {
+      dao.checkRepoVersion() must_== Full("0.0.2")
+    }
+  }
+
+  //  val postTmpl = Post(id = "?", parent = "", date = new ju.Date,
+  //    by = "Author", ip = "?.?.?.?", text = "")
+
+//    //val debatenoid = debate(id = "?")
+//    //val debatesaved = dao.create(tenantid, debatenoid).open_!
+//    //val debateloaded = dao.load(tenantid, debatesaved.id).open_!
+//    ////debatesaved.id must_== debateloaded.id
+}
+
+
+// vim: fdm=marker et ts=2 sw=2 tw=80 fo=tcqwn list
