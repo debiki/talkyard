a49bde3d52eec8825fcf235db9879549729ff243
Kaj Magnus Lindberg <kaj.magnus.lindberg@gmail.com>
2011-07-30 23:44:50 +0200

    Rename Dao to DaoSpi, add caching Dao
    Let the new Dao delegate database requests to a DaoSpi implementation,
    and cache pages in a ConcurrentMap.

--- src/main/scala/com/debiki/v0/Dao.scala
+++ src/main/scala/com/debiki/v0/Dao.scala
@@ -2,19 +2,20 @@
 
 package com.debiki.v0
 
-import net.liftweb.common.Box
+import com.debiki.v0.Prelude._
+import com.google.{common => guava}
+import java.{util => ju}
+import net.liftweb.common.{Logger, Box, Empty, Full, Failure}
 
-/** Debiki's Data Access Object interface.
+/** Debiki's Data Access Object service provider interface.
  */
-abstract class Dao {
+abstract class DaoSpi {
 
   def create(where: PagePath, debate: Debate): Box[Debate]
 
   def close()
 
   // COULD use:  save(Guid(tenantId, pageGuid), xs)  instead?
-  def save(tenantId: String, debateId: String, x: AnyRef): Box[AnyRef]
-
   def save[T](tenantId: String, debateId: String, xs: List[T]): Box[List[T]]
 
   def load(tenantId: String, debateId: String): Box[Debate]
@@ -29,5 +30,77 @@ abstract class Dao {
   /** Used as salt when hashing e.g. email and IP, before the hash
    *  is included in HTML. */
   def secretSalt(): String
+
+}
+
+/** Debiki's Data Access Object.
+ *
+ *  Delegates database requests to a DaoSpi implementation,
+ *  and caches pages in a ConcurrentMap.
+ */
+class Dao(impl: DaoSpi) {
+
+  private val _impl = impl
+  private case class Key(tenantId: String, debateId: String)
+
+  private val _cache: ju.concurrent.ConcurrentMap[Key, Debate] =
+      new guava.collect.MapMaker().
+          softValues().
+          maximumSize(100*1000).
+          //expireAfterWrite(10. TimeUnits.MINUTES).
+          makeComputingMap(new guava.base.Function[Key, Debate] {
+            def apply(k: Key): Debate = {
+              _impl.load(k.tenantId, k.debateId) openOr null
+            }
+          })
+
+  def create(where: PagePath, debate: Debate): Box[Debate] = {
+    for (debateWithIds <- _impl.create(where, debate)) yield {
+      val key = Key(where.tenantId, debateWithIds.guid)
+      val duplicate = _cache.putIfAbsent(key, debateWithIds)
+      errorIf(duplicate ne null, "Newly created page "+ safed(debate.guid) +
+          " found in mem cache [debiki_error_38WcK905]")
+      debateWithIds
+    }
+  }
+
+  def close() = _impl.close
+
+  def save[T](tenantId: String, debateId: String, xs: List[T]
+                 ): Box[List[T]] = {
+    for (xsWithIds <- _impl.save(tenantId, debateId, xs)) yield {
+      val key = Key(tenantId, debateId)
+      var replaced = false
+      while (!replaced) {
+        val oldPage = _cache.get(key)
+        val newPage = oldPage ++ xsWithIds
+        // newPage might == oldPage, if another thread just refreshed
+        // the page from the database.
+        replaced = _cache.replace(key, oldPage, newPage)
+        System.out.println("muu")
+      }
+      xsWithIds  // TODO return newPage instead? Or possibly a pair.
+    }
+  }
+
+  def load(tenantId: String, debateId: String): Box[Debate] = {
+    try {
+      Full(_cache.get(Key(tenantId, debateId)))
+    } catch {
+      case e: NullPointerException =>
+        Empty
+    }
+  }
+
+  def checkPagePath(pathToCheck: PagePath): Box[PagePath] =
+    _impl.checkPagePath(pathToCheck)
+
+  def checkAccess(pagePath: PagePath, userId: String, action: Action
+                     ): Option[IntrsAllowed] =
+    _impl.checkAccess(pagePath, userId, action)
+
+  def checkRepoVersion(): Box[String] = _impl.checkRepoVersion()
+
+  def secretSalt(): String = _impl.secretSalt()
 
 }
