ba559bc234d1c77b7cfde5992bb0b9d94808bf05
Kaj Magnus Lindberg <kaj.magnus.lindberg@gmail.com>
2012-02-22 05:20:34 +0100

    Lookup tenant, parse URL into PagePath, fix bad paths.

--- app/Global.scala
+++ app/Global.scala
@@ -3,28 +3,89 @@
  */
 
 
+import com.debiki.v0._
+import com.debiki.v0.Prelude._
+import debiki._
 import play.api._
+import play.api.mvc._
 
 
 object Global extends GlobalSettings {
 
   /**
-   * Query string based routing.
+   * Query string based routing and tenant ID lookup.
    */
-  override def onRouteRequest(
-    request: mvc.RequestHeader): Option[mvc.Handler] = {
+  override def onRouteRequest(request: RequestHeader): Option[Handler] = {
 
-    // Ignore the internal API and Javascript and CSS etcetera.
+    import DebikiHttp.{notFound, badRequest, redirect}
+
+    // Ignore the internal API and Javascript and CSS etcetera, in /-/.
     if (request.path startsWith "/-/")
       return super.onRouteRequest(request)
 
+    // Lookup tenant id in database. (Should cache it.)
+    val scheme = "http" // for now
+    val tenantId = DebikiDb.Dao.lookupTenant(scheme, request.host) match {
+      case found: FoundChost => found.tenantId
+      case found: FoundAlias => found.role match {
+        case TenantHost.RoleRedirect =>
+          return redirect(found.canonicalHostUrl + request.path)
+        case TenantHost.RoleLink => unimplemented("<link rel='canonical'>")
+        case _ =>
+          // lookupTenant should have returned FoundChost instead
+          // of FoundAlias with RoleCanonical/Duplicate.
+          assErr("DwE01k5Bk08")
+      }
+      case FoundNothing =>
+        return notFound("DwEI5F2", "The specified host name maps to no tenant.")
+    }
+
+    // Parse URL path: find folder, page id and slug.
+    val pagePath = PagePath.fromUrlPath(tenantId, request.path) match {
+      case PagePath.Parsed.Good(pagePath) => pagePath
+      case PagePath.Parsed.Bad(error) => return badRequest("DwE0kI3E4", error)
+      case PagePath.Parsed.Corrected(newPath) => return redirect(newPath)
+    }
+
+    // Find API version and main function in query string.
+    // Example: page?v0-reply-to=123 means version 0 and function `reply-to'.
+    val versionAndMainFun = request.rawQueryString.takeWhile(_ != '=')
+    // Later:
+    //val versionSeparator = versionAndMainFun.indexOf('-')
+    //val (versionPrefix, mainFun) =
+    // versionAndMainFun.splitAt(versionSeparator)
+    //val version = versionPrefix.drop(1).dropRight(1).toInt
+    // For now: (until I've updated all HTTP calls to include 'v0-')
+    val version = 0
+    val versionPrefix = ""
+    val mainFun = versionAndMainFun
+    
+    // Find parameters common to almost all requests.
+    val pageRoot: PageRoot =
+      request.queryString.get("view").map(rootPosts => rootPosts.size match {
+        case 1 => PageRoot(rootPosts.head)
+        // It seems this cannot hapen with Play Framework:
+        case 0 => assErr("DwE03kI8", "Query string param with no value")
+        case _ => return badRequest("DwE0k35", "Too many `view' values")
+      }) getOrElse PageRoot.TheBody
+
+    // Query string param value lookup.
+    def firstValueOf(param: String): Option[String] =
+      request.queryString.get(param).map(_.headOption).getOrElse(None)
+    def mainFunVal: String =
+      firstValueOf(versionAndMainFun) getOrElse ""
+
     // Route based on the query string.
-    val mainFun = request.rawQueryString.takeWhile(_ != '=')
     val App = controllers.Application
     val action = mainFun match {
-      case "edit" => App.editPost(request.path)
-      case "view" => App.viewPost(request.path)
-      case _ => return super.onRouteRequest(request)
+      case "edit" => App.editPost(pagePath, pageRoot, postId = mainFunVal)
+      case "view" => App.viewPost(pagePath, postId = mainFunVal)
+      case "feed" => App.feedNews(pagePath)
+      // If no main function specified:
+      case "" =>  App.viewPost(pagePath, postId = Page.BodyId)
+      // If invalid function specified:
+      case x => return badRequest(
+        "DwEQ435", "Bad query string main function: ?"+ x)
     }
     Some(action)
   }
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,26 @@
 package controllers
 
+import com.debiki.v0
+import com.debiki.v0.{PagePath, PageRoot}
+import com.debiki.v0.Prelude._
+import debiki._
+import net.liftweb.common.{Box, Full, Empty, Failure}
 import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
 
-  def editPost(pagePath: String) = Action {
-    Ok("editPost("+ pagePath +")")
+  def editPost(pathIn: PagePath, pageRoot: PageRoot, postId: String) = Action {
+    Ok("editPost("+ pathIn +", "+ pageRoot +", "+ postId +")")
   }
 
-  def viewPost(pagePath: String) = Action {
-    Ok("viewPost("+ pagePath +")")
+  def viewPost(pathIn: PagePath, postId: String) =
+        RedirectBadPath(pathIn) { implicit request =>
+    Ok("viewPost("+ pathIn +", "+ postId +")")
+  }
+
+  def feedNews(pathIn: PagePath) = Action {
+    Ok("feedNews("+ pathIn +")")
   }
 
   def callApi(apiPath: String) = Action {
@@ -19,6 +29,17 @@ object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("index = Action"))
+  }
+
+  def RedirectBadPath(pathIn: PagePath)(f: Request[AnyContent] => Result)
+        : Action[_] = Action { request =>
+    DebikiDb.Dao.checkPagePath(pathIn) match {
+      case Full(correct: PagePath) =>
+        if (correct.path == pathIn.path) f(request)
+        else Results.MovedPermanently(correct.path)
+      case Empty => Results.NotFound("404 Page not found: "+ pathIn.path)
+      case f: Failure => runErr("DwE03ki2", "Internal error"+ f.toString)
+    }
   }
 
 }
