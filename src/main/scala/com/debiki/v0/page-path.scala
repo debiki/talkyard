4c226bc77cc6f80cacaf0deb0b8fb9d4e1df4e31
Kaj Magnus Lindberg <kaj.magnus.lindberg@gmail.com>
2012-02-22 05:16:06 +0100

    Add object PagePath, which parses a PagePath from a URL
    Reuse code from debiki-app-lift.

--- src/main/scala/com/debiki/v0/page-path.scala
+++ src/main/scala/com/debiki/v0/page-path.scala
@@ -66,3 +66,85 @@ case class PagePath(  // COULD move to debate.scala.  Rename to RequestPath?
 }
 
 
+object PagePath {
+
+  sealed abstract class Parsed
+  object Parsed {
+    case class Good(value: PagePath) extends Parsed
+    case class Corrected(path: String) extends Parsed
+    case class Bad(error: String) extends Parsed
+  }
+
+  /**
+   * Parses the path part of a URL into a PagePath.
+   *
+   * URL path examples:
+   * - (server)/fold/ers/-pageId-page-name
+   * - (server)/fold/ers/page-name (here, the pageId is not shown in the path).
+   */
+  def fromUrlPath(tenantId: String, path: String): PagePath.Parsed = {
+    assert(path.head == '/')
+    val lastSlash = path.lastIndexOf('/')
+    val (folder, pageIdSlug) =
+      if (lastSlash != -1) path.splitAt(lastSlash + 1)
+      else (path, "")
+    assert(folder.head == '/' && folder.last == '/')
+    assert(!pageIdSlug.contains('/'))
+
+    // Check for bad folder paths.
+    folder match {
+      case _BadTrailingSlashRegex() =>
+        // Drop the trailing slash, to find the correct path.
+        assert(pageIdSlug isEmpty)
+        val folderAndPageIdName = path.dropRight(1)
+        return Parsed.Corrected(folderAndPageIdName)
+      case _BadHyphenRegex() =>
+        return Parsed.Bad("Hyphen in folder path")
+      case _ =>
+      // Fine
+    }
+
+    val (pageIdStr, pageSlug) = pageIdSlug match {
+      case "" => ("", "")
+      case _PageGuidAndSlugRegex(guid, name) => (guid, name)
+      case _PageGuidRegex(guid) => (guid, "")  // can result in an empty guid
+      case _PageSlugRegex(name) => ("", name)
+      case _PageGuidCorruptSlug(guid) => return Parsed.Bad("Bad page name")
+      case _ => return Parsed.Bad("Bad page id or name")
+    }
+    val (pageId, showId) =
+      if (pageIdStr isEmpty) (None, false)
+      else (Some(pageIdStr), true)
+
+    val pagePath = PagePath(tenantId = tenantId, folder = folder,
+      pageId = pageId, showId = showId, pageSlug = pageSlug)
+    Parsed.Good(pagePath)
+  }
+
+  // If a folder ends with .../-something/, then `something' is in fact
+  // a page guid and slug. The trailing slash should be removed.
+  private val _BadTrailingSlashRegex = """.*/-[^/]+/""".r
+
+  // Hyphens are not allowed in the folder path, because a hyphen means
+  // that a page guid follows. For example: /-folder/page  (invalid path)
+  private val _BadHyphenRegex = """.*/-.*""".r
+
+  // Note: PageGuidPtrn:
+  //  - The initial "-" is not part of the guid
+  //  - An empty guid means the guid is not yet known, which is the
+  //    case when you use this URL to request that a page be created
+  //    and that a guid be generated:  /parent/-?create
+  //      COULD: don't allow empty guids no more, not needed since
+  //      creating pages like so now: /folder/?createpage
+  // PageSlugPtrn:
+  //  - PageSlugPtrn: "*+~" have special meanings
+  //
+  private val _PageGuidPtrn = "-([a-z0-9_]*)"  // empty guid ok, read above
+  private val _PageSlugPtrn = "([^-*+~][^*+~]*)"
+  private val _PageGuidAndSlugRegex = (_PageGuidPtrn +"-"+ _PageSlugPtrn).r
+  private val _PageGuidRegex = _PageGuidPtrn.r
+  private val _PageSlugRegex = _PageSlugPtrn.r
+  // Catches corrupt page names iff used *after* PageGuidAndSlugRegex.
+  private val _PageGuidCorruptSlug = (_PageGuidPtrn +"[^a-z0-9_].*").r
+
+}
