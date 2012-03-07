72fed7d1378b5f58eeac0016f18ed6bc968de04d
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-03-07 18:39:21 +0100

    Add deployment scripts.

---
+++ deploy-11danny.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -x
+play=/mnt/data/dev/play/kajmagnus79/play
+
+set -u  # exit on unset variable
+set -e  # exit on non-zero command exit code
+set -o pipefail  # exit on false | true
+set -x
+
+function die {
+  echo "$1" >&2
+  exit 1
+}
+
+# Don't deploy unless everything has been committed.
+if [ ! -z "`git status --porcelain`" ]; then
+  echo 'There are dirty files. Please run git status, and add and commit.'
+  # exit 1
+fi
+
+# Build distribution.
+# First, obfuscate debiki.js:
+./yui.sh compress
+$play dist
+# Restore original debiki.js
+./yui.sh undo
+unzip -d dist/ dist/debiki-app-play-1.0-SNAPSHOT.zip
+
+ssh_ec2_user="ssh -i /home/kajmagnus/debiki/hosting/amazon/debiki-az-ec2-keypair-1.pem"
+ssh_play_user="ssh -i /home/kajmagnus/debiki/hosting/amazon/debiki-play-user.pem"
+host=dw0azirdbpv11danny
+basedir=/opt/debiki/debiki.se
+
+rsync -avz \
+  -e "$ssh_ec2_user"  \
+  ./dist/debiki-app-play-1.0-SNAPSHOT/* \
+  ec2-user@$host:$basedir/app1/
+
+# Allow user `play' to run `start'.
+$ssh_ec2_user \
+  ec2-user@$host \
+  "cd $basedir; chmod o+x app1/start"
+
+$ssh_play_user \
+  play@$host \
+  "cd $basedir; ./restart1.sh"
+
+
---
+++ yui.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# (Place  yuicompressor-2.4.6.jar  in scratch/, for this script to work.)
+
+set -eu
+
+set -x
+debikijs=modules/debiki-core/src/main/resources/toserve/js/debiki.js
+debikifull=modules/debiki-core/src/debiki.full.js
+
+function usageExit {
+  echo "Usage: $0 {compress|undo}"
+  exit 1
+}
+
+if [ "$#" -ne 1 ]; then
+  usageExit
+fi
+
+if [ "$1" = "undo" ]; then
+  mv "$debikifull" "$debikijs"
+  exit 0
+elif [ "$1" = "compress" ]; then
+  # Copy debiki.js to .full.js, and overwrite debiki.js with a compressed version.
+  cp "$debikijs" "$debikifull"
+  java -jar scratch/yuicompressor-2.4.6.jar --line-break 180 --charset utf8 -o "$debikijs" "$debikifull"
+else
+  usageExit
+fi
+
