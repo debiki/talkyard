c0d15dd8ba85eb578b18cca5d4634135876e1b6d
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-03-28 16:16:37 +0200

    Fix LoginRequest requirements; add LoginGrant helper functions.

--- src/main/scala/com/debiki/v0/Dao.scala
+++ src/main/scala/com/debiki/v0/Dao.scala
@@ -90,8 +90,16 @@ abstract class DaoSpi {
 object Dao {
   case class LoginRequest(login: Login, identity: Identity) {
     require(login.id startsWith "?")
-    require(identity.id startsWith "?")
-    require(identity.id == login.identityId)
+    require(login.identityId == identity.id)
+
+    // Only when you login via email, the identity id is already known
+    // (and is the email id).
+    if (identity.isInstanceOf[IdentityEmailId])
+      require(!identity.id.startsWith("?"))
+    else require(identity.id startsWith "?")
+
+    // The user id is not known before you have logged in.
+    require(identity.userId startsWith "?")
   }
 
   // COULD use RequesterInfo instead? (It includes role info too, is fine?)
@@ -101,6 +109,17 @@ object Dao {
     require(!user.id.contains('?'))
     require(login.identityId == identity.id)
     require(identity.userId == user.id)
+
+    def displayName: String = {
+      // (Somewhat dupl code: this also done in NiLo.displayName.)
+      if (user.displayName nonEmpty) user.displayName
+      else identity.displayName
+    }
+
+    def email: String = {
+      if (user.email nonEmpty) user.email
+      else identity.email
+    }
   }
 
 }
--- src/main/scala/com/debiki/v0/user.scala
+++ src/main/scala/com/debiki/v0/user.scala
@@ -61,8 +61,7 @@ class NiLo(people: People, val login: Login) {
   def user_! : User = people.user_!(identity_!.userId)
   def identity_! : Identity = people.identity_!(login.identityId)
   def displayName: String = {
-    // Duplicated code! This also done in LogInOut.loginSimple,
-    // when setting the nanme part of the SID cookie, in debiki-app-lift.
+    // (Somewhat dupl code: this also done in LoginGrant.displayName.)
     var n = user_!.displayName
     if (n nonEmpty) n else identity_!.displayName
   }
