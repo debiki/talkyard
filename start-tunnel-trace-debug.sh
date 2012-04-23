eba2c5adfa4d33e0af1e2eebbd6bdf8bb71aaa5f
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-04-23 15:36:23 +0200

    Make Play app listen port configurable.

--- start-tunnel-trace-debug.sh
+++ start-tunnel-trace-debug.sh
@@ -1,6 +1,11 @@
+if [ $# -ne 1 ]; then
+  echo 'Usage: $0 listen-port'
+  exit 1
+fi
+
 target/start \
   -Dconfig.resource=prod-tunnel.conf \
   -Dlogger.application=TRACE \
-  -Dhttp.port=9002 \
+  -Dhttp.port=$1 \
   -Xdebug -Xrunjdwp:transport=dt_socket,address=9999,server=y,suspend=n
 
