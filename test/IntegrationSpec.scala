e8ef2cfa97e12593bccc25986dd7ee3238d54287
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-04-23 15:37:33 +0200

    Add mostly broken and out commented Firefox browser tests.

---
+++ test/IntegrationSpec.scala
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
+ */
+
+package test
+
+import org.specs2.mutable._
+import play.api.test._
+import play.api.test.Helpers._
+//import fr.javafreelance.fluentlenium.core.filter.{FilterConstructor => FC}
+
+class BrowserFirefoxSpec extends Specification {
+
+  /*
+  "The browser Firefox" should {
+
+    "work from within a browser" in {
+      running(TestServer(3333), FIREFOX) { browser =>
+        browser.goTo("http://localhost:3333/")
+
+        browser.$("#dw-a-login").click();
+        browser.$("#dw-fi-login-name").text("Firefox")
+        val a = browser.$("#dw-fs-login-simple")
+        val b = browser.$("#dw-fs-login-simple").find(".ui-dialog-buttonset")
+        val c = browser.$("#dw-fs-login-simple").find(".ui-dialog-buttonset").find("button")
+        val d = browser.$("#dw-fs-login-simple").find(".ui-dialog-buttonset").find("button", FC.withText("Submit"))
+        d.click();
+
+        //browser.await().atMost(1000, java.util.concurrent.TimeUnit.)
+
+        browser.$(".dw-hor-a .dw-a-reply").click();
+
+        /*
+        browser.$("h1").first.getText must equalTo("Configure your 'Hello world':")
+
+        browser.$("#name").text("Bob")
+        browser.$("#submit").click()
+
+        browser.$("dl.error").size must equalTo(1)
+        browser.$("dl#repeat_field dd.error").first.getText must equalTo("Numeric value expected")
+        browser.$("#name").first.getValue must equalTo("Bob")
+
+        browser.$("#repeat").text("xxx")
+        browser.$("#submit").click()
+
+        browser.$("dl.error").size must equalTo(1)
+        browser.$("dl#repeat_field dd.error").first.getText must equalTo("Numeric value expected")
+        browser.$("#name").first.getValue must equalTo("Bob")
+        browser.$("#repeat").first.getValue must equalTo("xxx")
+
+        browser.$("#name").text("")
+        browser.$("#submit").click()
+
+        browser.$("dl.error").size must equalTo(2)
+        browser.$("dl#name_field dd.error").first.getText must equalTo("This field is required")
+        browser.$("dl#repeat_field dd.error").first.getText must equalTo("Numeric value expected")
+        browser.$("#name").first.getValue must equalTo("")
+        browser.$("#repeat").first.getValue must equalTo("xxx")
+
+        browser.$("#name").text("Bob")
+        browser.$("#repeat").text("10")
+        browser.$("#submit").click()
+
+        browser.$("header a").first.getText must equalTo("Here is the result:")
+
+        val items = browser.$("section ul li")
+
+        items.size must equalTo(10)
+        items.get(0).getText must equalTo("Hello Bob!")
+
+        browser.$("p.buttons a").click()
+        */
+
+        browser.$("h1").first.getText must equalTo("Configure your 'Hello world':")
+
+      }
+    }
+    
+  }
+  */
+  
+}
