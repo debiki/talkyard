63c7cc3a839546a96aa2748e01b7d34ee38a1a7b
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-07-29 00:54:52 +0200

    Move comments, e.g. naming notes, to readme.txt.

--- app/assets/res/debiki.js
+++ app/assets/res/debiki.js
@@ -1,59 +1,5 @@
 /* Copyright (c) 2010 - 2012 Kaj Magnus Lindberg. All rights reserved. */
 
-// In this file:
-// - jQuery extension functions, prefixed with "dw" to avoid name clashes
-// - The implementation of the Debiki module
-// - A jQuery onload handler
-
-// Google Closure Linter: Run like so:
-//  gjslint src/main/resources/toserve/js/debiki.js | egrep -v 'E:0002:'
-
-/*{{{ Bug avoidance notes
-
-For an <a>, use this.hash not $(this).attr('href'), because in IE 7
-attr() prepends 'http://server/.../page' to the href.  Related:
-  http://goo.gl/OF16Q  — the JavaScript Bible page 603
-  http://webmasters.stackexchange.com/questions/20621/
-                    okay-to-use-the-hash-dom-node-property
-
-}}}*/
-/* {{{ Misc naming notes
-
- dwCoSid:
-  "dw" is a prefix, avoids name clashes.
-  "Co" means "Cookie".
-  "Sid" is the cookie name.
-
- dwEvLoggedInOut:
-  "Ev" means "event".
-  "LoggedInOut" is the event.
-
- So you can do: grep dwCo, grep dwEv
-
- HTML5 data attributes names:  ??
- Like the CSS class names, but underscore not hyphen, so as to aovid
- uppercase/hyphen conversion. (E.g. 'data-variable-name' is converted to
- variableName (no hyphen, uppercase 'N'), and back, according to the html5
- spec: <http://www.w3.org/TR/html5/elements.html#
-          embedding-custom-non-visible-data-with-the-data-attributes>
- Using underscoer ensures the data names in the html doc matches
- the names in the Javascript source code which avoids confusion.
- Example:  zd_t_id  means:  folded (zd, 'z' is fold)  thread (t)  id (id).
- But don't use:  zd-t-id, that'd be converted to 'zdTId' I think.
- If the data is only set and read via Javascript (never serialized to html),
- then please use 'dwDataName' (then you know you need only consider the
- javascript files (this file) should you want to rename it).)
-
- Let names of functions that return a jQuery object end with $.
- Let names of functions that should be passed to jQuery.each start with $.
- Let jQuery objects start with $.
- Currently jQuery extensions are prefixed by 'dw', e.g. $post.dwAuthorId().
- Example:
-   var $header = d.i.findPostHeader$(postId);
-   $header.each($doSomething);
-
-}}}*/
-
 
 //========================================
    (function(){
@@ -64,11 +10,8 @@ attr() prepends 'http://server/.../page' to the href.  Related:
 
 "use strict";
 
-// Import namespaces as `d.i` and `d.u`.
 var d = { i: debiki.internal, u: debiki.v0.util };
 
-
-// ------- Variables
 
 // Debiki convention: Dialog elem tabindexes should vary from 101 to 109.
 // HTML generation code assumes this, too. See Debiki for Developers, #7bZG31.
@@ -88,9 +31,6 @@ var SVG = nativeSvgSupport && document.URL.indexOf('svg=false') === -1 ?
 
 var Me = d.i.makeCurUser();
 
-
-
-// ------- Posts
 
 
 // Inits a post and its parent thread.
@@ -154,8 +94,6 @@ function $initPostStep2() {
 
 
 
-// -------
-
 
 function confirmClosePage() {
   // If there're any reply forms with non-empty replies (textareas),
@@ -176,8 +114,6 @@ function confirmClosePage() {
 };
 
 
-
-// ------- Initialization functions
 
 
 function registerEventHandlersFireLoginOut() {
