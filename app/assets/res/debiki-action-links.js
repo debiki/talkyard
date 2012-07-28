e1c011d6fd7305f142fe1f9ca4eec0f3a2a2273e
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-07-28 23:43:45 +0200

    Break out action link init functions
    These: createActionLinksForPost and shohwActionLinksOnHoverPost.

--- app/assets/res/debiki-action-links.js
+++ app/assets/res/debiki-action-links.js
@@ -7,6 +7,83 @@ var d = { i: debiki.internal, u: debiki.v0.util };
 var $ = d.i.$;
 
 
+d.i.createActionLinksForPost = function(post) {
+  var $post = $(post).dwCheckIs('.dw-p');
+  var $thread = $post.closest('.dw-t');
+
+  // Find or add action buttons.
+  var $actions = $thread.children('.dw-res').children('.dw-p-as');
+  if ($actions.length) {
+    // This thread is laid out horizontally and the action links have
+    // already been placed somewhere in the child thread <ol>.
+  } else {
+    $actions = $('#dw-action-menu')
+        .clone()
+        .removeAttr('id')
+        .css('visibility', 'hidden');
+    $thread.find('> .dw-as').replaceWith($actions);
+    // Touch devices cannot show-on-mouse-hover.
+    if (Modernizr.touch)
+      $actions.children('.dw-a-reply, .dw-a-rate')
+          .css('visibility', 'visible');
+  }
+
+  // {{{ On delegating events for reply/rate/edit.
+  // Placing a jQuery delegate on e.g. .debiki instead, entails that
+  // these links are given excessively low precedence on Android:
+  // on a screen touch, any <a> nearby that has a real click event
+  // is clicked instead of the <a> with a delegate event. The reply/
+  // reply/rate/edit links becomes virtually unclickable (if event
+  // delegation is used instead). }}}
+  $actions.children('.dw-a-reply').click(d.i.$showReplyForm);
+  $actions.children('.dw-a-rate').click(d.i.$showRatingForm);
+  $actions.children('.dw-a-more').click(function() {
+    $(this).closest('.dw-p-as').find('.dw-a')
+        .show()
+        .end().end().remove();
+  });
+  //$actions.children('.dw-a-link').click($showLinkForm); — not implemented
+  $actions.children('.dw-a-edit').click(d.i.$showEditsDialog);
+  $actions.children('.dw-a-flag').click(d.i.$showFlagForm);
+  $actions.children('.dw-a-delete').click(d.i.$showDeleteForm);
+};
+
+
+d.i.shohwActionLinksOnHoverPost  = function(post) {
+  var $thread = $(post).dwCheckIs('.dw-p').closest('.dw-t');
+  var $post = $thread.filter(':not(.dw-depth-0)').children('.dw-p');
+
+  // When hovering a post, show actions (except for the root post reply link,
+  // which is always visible).
+  // (Better avoid delegates for frequent events such as mouseenter.)
+  $post.mouseenter(function() {
+    var $i = $(this);
+
+    // If actions are already shown for an inline child post, ignore event.
+    // (Sometimes the mouseenter event is fired first for an inline child
+    // post, then for its parent — and then actions should be shown for the
+    // child post; the parent should ignore the event.)
+    var inlineChildActionsShown = $i.find('#dw-p-as-shown').length;
+
+    // If the post is being edited, show no actions.
+    // (It's rather confusing to be able to Reply to the edit <form>.)
+    var isBeingEdited = $i.children('.dw-f-e:visible').length;
+
+    if (isBeingEdited)
+      d.i.hideActions();
+    else if (!inlineChildActionsShown)
+      $i.each(d.i.$showActions);
+    // else leave actions visible, below the inline child post.
+  });
+
+  $thread.mouseleave(function() {
+    // If this is an inline post, show the action menu for the parent post
+    // since we're hovering that post now.
+    $(this).closest('.dw-p').each(d.i.$showActions);
+  });
+};
+
+
 // Shows actions for the current post, or the last post hovered.
 d.i.$showActions = function() {
   // Hide any action links already shown; show actions for one post only.
--- app/assets/res/debiki.js
+++ app/assets/res/debiki.js
@@ -157,83 +157,15 @@ function $initPostsThread() {
 
 
 function $initPostsThreadStep1() {
-  var $thread = $(this).closest('.dw-t');
-
-  // Find or add action buttons.
-  var $actions = $thread.children('.dw-res').children('.dw-p-as');
-  if ($actions.length) {
-    // This thread is laid out horizontally and the action links have
-    // already been placed somewhere in the child thread <ol>.
-  } else {
-    $actions = $('#dw-action-menu')
-        .clone()
-        .removeAttr('id')
-        .css('visibility', 'hidden');
-    $thread.find('> .dw-as').replaceWith($actions);
-    // Touch devices cannot show-on-mouse-hover.
-    if (Modernizr.touch)
-      $actions.children('.dw-a-reply, .dw-a-rate')
-          .css('visibility', 'visible');
-  }
-
-  // {{{ On delegating events for reply/rate/edit.
-  // Placing a jQuery delegate on e.g. .debiki instead, entails that
-  // these links are given excessively low precedence on Android:
-  // on a screen touch, any <a> nearby that has a real click event
-  // is clicked instead of the <a> with a delegate event. The reply/
-  // reply/rate/edit links becomes virtually unclickable (if event
-  // delegation is used instead). }}}
-  $actions.children('.dw-a-reply').click(d.i.$showReplyForm);
-  $actions.children('.dw-a-rate').click(d.i.$showRatingForm);
-  $actions.children('.dw-a-more').click(function() {
-    $(this).closest('.dw-p-as').find('.dw-a')
-        .show()
-        .end().end().remove();
-  });
-  //$actions.children('.dw-a-link').click($showLinkForm); — not implemented
-  $actions.children('.dw-a-edit').click(d.i.$showEditsDialog);
-  $actions.children('.dw-a-flag').click(d.i.$showFlagForm);
-  $actions.children('.dw-a-delete').click(d.i.$showDeleteForm);
-
+  d.i.createActionLinksForPost(this);
   // Open/close threads if the fold link is clicked.
+  var $thread = $(this).closest('.dw-t');
   $thread.children('.dw-z').click(d.i.$threadToggleFolded);
 };
 
 
-// Things that can be done a while after page load.
 function $initPostsThreadStep2() {
-  var $thread = $(this).closest('.dw-t');
-  var $post = $thread.filter(':not(.dw-depth-0)').children('.dw-p');
-
-  // When hovering a post, show actions, and make it resizable.
-  // But always show the leftmost Reply, at depth-0, that creates a new column.
-  // (Better avoid delegates for frequent events such as mouseenter.)
-  $post.mouseenter(function() {
-    var $i = $(this);
-
-    // If actions are already shown for an inline child post, ignore event.
-    // (Sometimes the mouseenter event is fired first for an inline child
-    // post, then for its parent — and then actions should be shown for the
-    // child post; the parent should ignore the event.)
-    var inlineChildActionsShown = $i.find('#dw-p-as-shown').length;
-
-    // If the post is being edited, show no actions.
-    // (It's rather confusing to be able to Reply to the edit <form>.)
-    var isBeingEdited = $i.children('.dw-f-e:visible').length;
-
-    if (isBeingEdited)
-      d.i.hideActions();
-    else if (!inlineChildActionsShown)
-      $i.each(d.i.$showActions);
-    // else leave actions visible, below the inline child post.
-  });
-
-  $thread.mouseleave(function() {
-    // If this is an inline post, show the action menu for the parent post
-    // since we're hovering that post now.
-    $(this).closest('.dw-p').each(d.i.$showActions);
-  });
-
+  d.i.shohwActionLinksOnHoverPost(this);
   $initPostStep1.apply(this);
 };
 
