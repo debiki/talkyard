22ebfceed980fc5d5dd92d9f7039857bcd57b209
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-08-08 09:23:40 +0200

    Start Brunch background process.

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+
+# Have Brunch (a NodeJS project) compile and bundle scripts and styles
+# from client/ to Play's public/ dir.
+brunch watch -c conf/nodejs-brunch-config.ls &
+
 /mnt/data/dev/play/github/play $@
 
