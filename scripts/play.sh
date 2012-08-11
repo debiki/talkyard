c7416ebebe4ce86aab78af149d3015f64507bbaf
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-08-11 20:28:37 +0200

    Move Brunch config files to conf-client/.

--- scripts/play.sh
+++ scripts/play.sh
@@ -2,7 +2,7 @@
 
 # Have Brunch (a NodeJS project) compile and bundle scripts and styles
 # from client/ to Play's public/ dir.
-brunch watch -c conf/nodejs-brunch-config.ls &
+brunch watch -c conf-client/nodejs-brunch-config.ls &
 
 /mnt/data/dev/play/github/play $@
 
