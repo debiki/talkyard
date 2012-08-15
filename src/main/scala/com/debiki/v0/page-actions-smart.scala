ceb05974db1ee5eb65d0a1e3f99d060a4c58193f
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-08-15 15:41:33 +0200

    Break out actions and smart/virtual actions to separate files.

---
+++ src/main/scala/com/debiki/v0/page-actions-smart.scala
@@ -0,0 +1,197 @@
+// Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
+
+package com.debiki.v0
+
+import java.{util => ju}
+import collection.{immutable => imm, mutable => mut}
+import Prelude._
+import Debate._
+import FlagReason.FlagReason
+
+
+// COULD rename all these ViAc/NiPo/whatever to SmartAction/Post/Whatever.
+// COULD take an Action subclass type param, so it'd be possible to use e.g. a
+// SmartPageAction[Rating].
+/** A virtual Action, that is, an Action plus some utility methods that
+ *  look up other stuff in the relevant Debate.
+ */
+class ViAc(val debate: Debate, val action: Action) {
+  def id: String = action.id
+  def ctime = action.ctime
+  def login: Option[Login] = debate.login(action.loginId)
+  def login_! : Login = login.getOrElse(runErr(
+     "DwE6gG32", "No login with id "+ safed(action.loginId) +
+     " for action "+ safed(id)))
+  def identity: Option[Identity] = login.flatMap(l =>
+                                    debate.identity(l.identityId))
+  def identity_! : Identity = debate.identity_!(login_!.identityId)
+  def user : Option[User] = identity.flatMap(i => debate.user(i.userId))
+  def user_! : User = debate.user_!(identity_!.userId)
+  def ip: Option[String] = action.newIp.orElse(login.map(_.ip))
+  def ip_! : String = action.newIp.getOrElse(login_!.ip)
+  def ipSaltHash: Option[String] = ip.map(saltAndHashIp(_))
+  def ipSaltHash_! : String = saltAndHashIp(ip_!)
+
+  def metaPosts = debate.metaFor(this)
+  lazy val metaText: String = metaPosts.foldLeft("")(_ + _.text)
+
+  def isTreeDeleted = {
+    // In case there are > 1 deletions, consider the first one only.
+    // (Once an action has been deleted, it isn't really possible to
+    // delete it again in some other manner? However non transactional
+    // (nosql) databases might return many deletions? and we should
+    // care only about the first.)
+    firstDelete.map(_.wholeTree) == Some(true)
+  }
+
+  def isDeleted = deletions nonEmpty
+
+  // COULD optimize this, do once for all posts, store in map.
+  lazy val deletions = debate.deletions.filter(_.postId == action.id)
+
+  /** Deletions, the most recent first. */
+  lazy val deletionsSorted = deletions.sortBy(- _.ctime.getTime)
+
+  lazy val lastDelete = deletionsSorted.headOption
+  lazy val firstDelete = deletionsSorted.lastOption
+}
+
+/** A Virtual Post into account all edits applied to the actual post.
+ */
+class ViPo(debate: Debate, val post: Post) extends ViAc(debate, post) {
+  def parent: String = post.parent
+  def tyype = post.tyype
+  // def ctime = lastEditApp.map(ea => toIso8601(ea.ctime))
+  lazy val (text: String, markup: String) = textAndMarkupAsOf(Long.MaxValue)
+
+  /** Applies all edits up to, but not including, the specified date.
+   *  Returns the resulting text and markup.
+   *  Keep in sync with textAsOf in debiki.js.
+   *    COULD make textAsOf understand changes in markup type,
+   *    or the preview won't always work correctly.
+   */
+  def textAndMarkupAsOf(millis: Long): (String, String) = {
+    var curText = post.text
+    var curMarkup = post.markup
+    val dmp = new name.fraser.neil.plaintext.diff_match_patch
+    for ((edit, eapp) <- editsAppdAsc; if eapp.ctime.getTime < millis) {
+      curMarkup = edit.newMarkup.getOrElse(curMarkup)
+      val patchText = edit.text
+      if (patchText nonEmpty) {
+        // COULD check [1, 2, 3, …] to find out if the patch applied
+        // cleanaly. (The result is in [0].)
+        type P = name.fraser.neil.plaintext.diff_match_patch.Patch
+        val patches: ju.List[P] = dmp.patch_fromText(patchText) // silly API, ...
+        val p2 = patches.asInstanceOf[ju.LinkedList[P]] // returns List but needs...
+        val result = dmp.patch_apply(p2, curText) // ...a LinkedList
+        val newText = result(0).asInstanceOf[String]
+        curText = newText
+      }
+    }
+    (curText, curMarkup)
+  }
+
+  def textInitially: String = post.text
+  def where: Option[String] = post.where
+  def edits: List[Edit] = debate.editsFor(post.id)
+  // COULD rename editsAppdRevd to editsAppdRevtd (Revtd instead of Revd).
+  lazy val (editsDeleted, editsPending, editsAppdDesc, editsAppdRevd) = {
+    var deleted = List[(Edit, Delete)]()
+    var pending = List[Edit]()
+    var applied = List[(Edit, EditApp)]()
+    var appdRevd = List[(Edit, EditApp, Delete)]()
+    edits foreach { e =>
+      val del = debate.deletionFor(e.id)
+      if (del isDefined) {
+        deleted ::= e -> del.get
+      } else {
+        var eappsLive = List[(Edit, EditApp)]()
+        var eappsDeleted = List[(Edit, EditApp, Delete)]()
+        val eapps = debate.editAppsByEdit(e.id)
+        eapps foreach { ea =>
+          val del = debate.deletionFor(ea.id)
+          if (del isEmpty) eappsLive ::= e -> ea
+          else eappsDeleted ::= ((e, ea, del.get))
+        }
+        if (eappsLive isEmpty) pending ::= e
+        else applied ::= eappsLive.head
+        if (eappsDeleted nonEmpty) {
+          appdRevd :::= eappsDeleted
+        }
+      }
+    }
+
+    // Sort by 1) deletion, 2) application, 3) edit creation, most recent
+    // first.
+    (deleted.sortBy(- _._2.ctime.getTime),
+      pending.sortBy(- _.ctime.getTime),
+      applied.sortBy(- _._2.ctime.getTime),
+      appdRevd.sortBy(- _._3.ctime.getTime))
+  }
+
+  def editsAppdAsc = editsAppdDesc.reverse
+
+  lazy val lastEditApp = editsAppdDesc.headOption.map(_._2)
+
+  /** Whether or not this Post has been published.
+   *
+   *  If the root post is published, then the whole page is published.
+   *  If a comment is published, then it's been approved (it's not spam).
+   */
+  lazy val publd: Option[Boolean] = {
+    if (publs isEmpty) None
+    else Some(true)  // for now, don't consider deletions of publications
+  }
+
+  /** Only the first (w.r.t. its ctime) non-deleted publication has any effect.
+   */
+  lazy val publs: List[Post] = debate.publsByParentId(id)
+
+  // COULD optimize this, do once for all flags.
+  lazy val flags = debate.flags.filter(_.postId == post.id)
+
+  lazy val lastFlag = flags.sortBy(- _.ctime.getTime).headOption
+
+  lazy val flagsByReason: imm.Map[FlagReason, List[Flag]] = {
+    // Add reasons and flags to a mutable map.
+    var mmap = mut.Map[FlagReason, mut.Set[Flag]]()
+    for (f <- flags)
+      mmap.getOrElse(f.reason, {
+        val s = mut.Set[Flag]()
+        mmap.put(f.reason, s)
+        s
+      }) += f
+    // Copy to an immutable version.
+    imm.Map[FlagReason, List[Flag]](
+      (for ((reason, flags) <- mmap)
+      yield (reason, flags.toList)).toList: _*)
+  }
+
+  /** Pairs of (FlagReason, flags-for-that-reason), sorted by
+   *  number of flags, descending.
+   */
+  lazy val flagsByReasonSorted: List[(FlagReason, List[Flag])] = {
+    flagsByReason.toList.sortWith((a, b) => a._2.length > b._2.length)
+  }
+
+  private val _FixPosRegex = """fixed-position: ?(\d+)""".r
+
+  lazy val meta: PostMeta = {
+    var fixedPos: Option[Int] = None
+    var isArticleQuestion = false
+    for (m <- metaPosts ; line <- m.text.lines) line match {
+      case "article-question" => isArticleQuestion = true
+      case _FixPosRegex(pos) => fixedPos = Some(pos.toInt)
+      case _ =>
+    }
+    PostMeta(isArticleQuestion = isArticleQuestion, fixedPos = fixedPos)
+  }
+}
+
+class ViEd(debate: Debate, val edit: Edit) extends ViAc(debate, edit) {
+
+}
+
+
+// vim: fdm=marker et ts=2 sw=2 tw=80 fo=tcqwn list
+
---
+++ src/main/scala/com/debiki/v0/page-actions.scala
@@ -0,0 +1,313 @@
+// Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
+
+package com.debiki.v0
+
+import java.{util => ju}
+import collection.{immutable => imm, mutable => mut}
+import Prelude._
+import Debate._
+import FlagReason.FlagReason
+
+
+sealed abstract class Action {  // COULD delete, replace with Post:s?
+  /** A local id, unique only in the Debate that this action modifies.
+    * "?" means unknown.
+    */
+  def id: String
+  require(id != "0")
+  require(id nonEmpty)
+
+  /** An id that identifies the login session, which in turn identifies
+   *  the user and IP and session creation time.
+   */
+  def loginId: String
+
+  /** Always None, unless the post was sent from somewhere else than
+   *  the relevant Login.ip.
+   */
+  def newIp: Option[String]
+  def ctime: ju.Date
+
+  def textLengthUtf8: Int = 0
+}
+
+/** Classifies an action, e.g. tags a Post as being "interesting" and "funny".
+ *
+ *  If you rate an action many times, only the last rating counts.
+ *  - For an authenticated user, his/her most recent rating counts.
+ *  - For other users, the most recent rating for the login id / session id
+ *    counts.
+ *  - Could let different non-authenticated sessions with the same
+ *    user name, ip and email overwrite each other's ratings.
+ *    But I might as well ask them to login instead? Saves my time, and CPU.
+ */
+case class Rating (
+  id: String,
+  postId: String,
+  loginId: String,
+  newIp: Option[String],
+  ctime: ju.Date,
+  tags: List[String]
+) extends Action
+
+
+/** Info on all ratings on a certain action, grouped and sorted in
+ *  various manners.
+ */
+// COULD rename to RatingsOnAction?
+abstract class SingleActionRatings {
+
+  /** The most recent rating, by authenticated users. */
+  def mostRecentByUserId: collection.Map[String, Rating]
+
+  /** The most recent rating, by non authenticated users. */
+  // COULD rename to ...ByGuestId
+  def mostRecentByNonAuLoginId: collection.Map[String, Rating]
+
+  /** The most recent ratings, for all non authenticated users,
+   *  grouped by IP address.
+   */
+  // COULD rename to ...ByIdtyId
+  def allRecentByNonAuIp: collection.Map[String, List[Rating]]
+
+  /** The most recent version of the specified rating.
+   *  When you rate an action a second time, the most recent rating
+   *  overwrites the older one.
+   */
+  def curVersionOf(rating: Rating): Rating
+}
+
+
+object FlagReason extends Enumeration {
+  type FlagReason = Value
+  val Spam, Illegal, /* Copyright Violation */ CopyVio, Other = Value
+}
+
+case class Flag(
+  id: String,
+  postId: String,
+  loginId: String,
+  newIp: Option[String],
+  ctime: ju.Date,
+  reason: FlagReason,
+  details: String
+) extends Action {
+  override def textLengthUtf8: Int = details.getBytes("UTF-8").length
+}
+
+sealed abstract class PostType  // rename to ActionType?
+object PostType {
+
+  /** A blog post, or forum questiom or comment. */
+  case object Text extends PostType
+
+  /** Edits a Post.text. */
+  //case object Edit extends PostType
+
+  /** Makes an Action suggestion take effect.
+   */
+  case object Publish extends PostType
+
+  /** Meta information describes another Post. */
+  // COULD use dedicated PostType:s instead, then the computer/database
+  // would understand what is it (but it'd have to parse the Meta.text to
+  // understand what is it.
+  case object Meta extends PostType
+
+  /** Deletes something, e.g. a post.
+   *
+   * Instead of the deleted thing information that the thing
+   * was deleted is shown, e.g. "3 comments deleted by <someone>".
+   */
+  // case object Deletion extends PostType
+
+  /** Undoes an action, as if had it never been done.
+   *
+   * This is different from Delete, because if you delete a post,
+   * information is shown on the page that here is a deleted post.
+   * However, if you Undo a post, there'll be not a trace of it
+   * on the generated page.
+   * Initially, though, only Undo:s of Deletion:s and Flag:s will be
+   * implemented (so you can undelete an unflag stuff).
+   */
+  // case object Undo extends PostType
+
+  //sealed abstract trait FlagReason
+  //case object FlagSpam extends PostType with FlagReason
+}
+
+case class Post(  // COULD merge all actions into Post,
+                  // and use different PostType:s (which would include
+                  // the payload) for various different actions.
+                  // And rename to ... Action? PageAction?
+
+  // is "?" if unknown, or e.g. "?x" if it's unknown.
+  // COULD replace w case classes e.g. IdKnown(id)/IdPending(tmpId)/IdUnknown,
+  // then it would not be possible to forget to check for "?".
+  id: String,
+  parent: String,
+  ctime: ju.Date,
+  loginId: String,
+  newIp: Option[String],
+  text: String,
+
+  /** The markup language to use when rendering this post.
+   */
+  markup: String,
+
+  tyype: PostType,
+
+  /** If defined, this is an inline comment and the value
+   *  specifies where in the parent post it is to be placed.
+   */
+  where: Option[String] = None   // COULD move to separate Meta post?
+                                 // Benefits: Editing and versioning of
+                                 // `where', without affecting this Post.text.
+                                // Benefit 2: There could be > 1 meta-Where
+                                // for each post, so you could make e.g. a
+                                // generic comment that results in ...
+                                // ...?? arrows to e.g. 3 other comments ??
+) extends Action {
+  override def textLengthUtf8: Int = text.getBytes("UTF-8").length
+}
+
+case class PostMeta(
+  isArticleQuestion: Boolean = false,
+  fixedPos: Option[Int] = None
+)
+
+// Could rename to Patch? or Diff?
+// SHOULD merge into Post and PostType.Edit
+case class Edit (
+  id: String,
+  postId: String,
+  ctime: ju.Date,
+  loginId: String,
+  newIp: Option[String],
+  text: String,
+
+  /** Changes the markup henceforth applied to postId's text.
+   *
+   * None means reuse the current markup.
+   */
+  newMarkup: Option[String]
+) extends Action {
+  override def textLengthUtf8: Int = text.getBytes("UTF-8").length
+}
+
+
+/** Edit applications (i.e. when edits are applied).
+ *
+ *  COULD make generic: applying a Post means it's published.
+ *  Applying an Edit means the relevant Post is edited.
+ *  Applying a Flag means the relevant Post is hidden.
+ *  Applying a Delete means whatever is whatever-happens-when-it's-deleted.
+ *  And each Action could have a applied=true/false field?
+ *  so they can be applied directly on creation?
+ */
+// SHOULD merge into Post and use case class PostType.Appl?
+case class EditApp(   // COULD rename to Appl?
+  id: String,
+  editId: String,  // COULD rename to actionId?
+  loginId: String,
+  newIp: Option[String],
+  ctime: ju.Date,
+
+  /** The text after the edit was applied. Needed, in case an `Edit'
+   *  contains a diff, not the resulting text itself. Then we'd better not
+   *  find the resulting text by applying the diff to the previous
+   *  version, more than once -- that wouldn't be future compatible: the diff
+   *  algorithm might functioni differently depending on software version
+   *  (e.g. because of bugs).
+   *  So, only apply the diff algorithm once, to find the EddidApplied.result,
+   *  and thereafter always use EditApplied.result.
+   *
+   *  Update: What! I really don't think the diff alg will change in the
+   *  future! Anyone changing the diff file format would be insane,
+   *  and would not have been able to write a diff alg at all.
+   *  So `result' is *not* needed, except for perhaps improving performance.
+   *
+   *  COULD change to an Option[String] and define it only one time out of ten?
+   *  Then the entire post would be duplicated only 1 time out of 10 times,
+   *  and that would hardly affect storage space requirements,
+   *  but at the same time improving performance reasonably much ??
+   *  Or store cached post texts in a dedicated db table.
+   */
+  result: String
+) extends Action
+
+/** Deletes an action. When actionId (well, postId right now)
+ *  is a post, it won't be rendered. If `wholeTree', no reply is shown either.
+ *  If actionId is an EditApp, that edit is undone. BAD??! Too complicated??
+ *    What does it mean to Delete an EditApp Delete:ion?
+ *    Of course it would mean that the Deletion was undone,
+ *    that is, Undo, that is, the EditApp would be in effect again,
+ *    that is, the Edit would be in effect again.
+ *    However I think this is too complicated.
+ *    Perhaps it's easier to introduce a Revert class.
+ *    And if there are many Apply:s and Revert:s for an Edit,
+ *    then the most recent Apply or Revert is in effect.
+ *    Benefit: You'd never need to walk along a chain of Delete:s,
+ *    to find out which EditApp or Post was actually deleted.
+ *    ????
+ *  --- Not implemented: --------
+ *  If `wholeTree', all edit applications from actionId and up to
+ *  delete.ctime are undone.
+ *  If actionId is an Edit, the edit will no longer appear in the list of
+ *  edits you can choose to apply.
+ *  If `wholeTree', no Edit from actionId up to delete.ctime will appear
+ *  in the list of edits that can be applied.
+ *  -----------------------------
+ */
+// SHOULD merge into Post and use case class PostType.Deletion?
+case class Delete(
+  id: String,
+  postId: String,  // COULD rename to actionId
+  loginId: String,
+  newIp: Option[String],
+  ctime: ju.Date,
+  wholeTree: Boolean,  // COULD rename to `recursively'?
+  reason: String  // COULD replace with a Post that is a reply to this Delete?
+) extends Action {
+  override def textLengthUtf8: Int = reason.getBytes("UTF-8").length
+}
+
+
+// COULD make a Deletion class, and a DelApp (deletion application) class.
+// Then sometimes e.g. 3 people must delete a post for it to really
+// be deleted. Perhaps DelSug = deletion suggestion?
+// And rename Edit to EditSug?
+// EditSug & EditApp, + DelSug & DelApp - or join these 4 classes to 2 generic?
+// What about Flag and FlagApp? Eventually if flagged as spam e.g. 2 times
+// then the post would be automatically hidden?
+// What about only one ActionApp class, that applies the relevant action?
+// But then e.g. EditApp.result (resulting text) would be gone!?
+// And DelApp.wholeTree ... and FlagApp.reason + details.
+// Perhaps better have many small specialized classes.
+
+
+
+// Should probably move to debiki-app-play (since knows exactly where
+// debiki-app-play places it, in the URL).
+case class ActionLocator(
+  pagePath: PagePath,
+  actionId: String,
+  actionCtime: ju.Date,
+  actionType: String  // for now. later: PostType? renamed to ActionType?
+){
+
+  def path: String = {
+    // - Add `?view=3` for templates, since they're on their own virtual page
+    // not connected to the root post.
+    // - Add `?view` to paths that end with .js or .css or Debiki will
+    // render the page as text, not html.
+    val fragAndQuery =
+      if (actionId == Page.TemplateId) pagePath.path +"?view="+ Page.TemplateId
+      else if (pagePath.isCodePage) pagePath.path + "?view"
+      else pagePath.path
+    fragAndQuery +"#post-"+ actionId
+  }
+}
+
+
+// vim: fdm=marker et ts=2 sw=2 tw=80 fo=tcqwn list
