a999b857956d675a4a6d5e2579d2510ad474711d
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-08-30 09:37:02 +0200

    Break out compile-client.sh from scripts/play.sh.

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Have Brunch (a NodeJS project) compile and bundle scripts and styles
-# from client/ to Play's public/ dir.
-brunch watch -c conf-client/nodejs-brunch-config.ls &
-
 /mnt/data/dev/play/github/play $@
 
