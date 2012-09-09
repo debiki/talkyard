4f742fcb7ff3370b3e36761de44f0da15e611c03
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-09-09 21:46:28 +0200

    Have AutoApprover analyze a user's previous actions
    And add some test cases.

--- app/debiki/AutoApprover.scala.scala
+++ app/debiki/AutoApprover.scala.scala
@@ -14,12 +14,113 @@ import Prelude._
  */
 object AutoApprover {
 
+  private[debiki] val Limit = 15
+
+  val NumFirstCommentsToApprovePreliminarily = 2
+  val TooManyUnreviewedComments = 10
 
   def perhapsApprove(pageReq: PageRequest[_]): Option[Approval] = {
     if (pageReq.user_!.isAdmin)
       Some(Approval.AuthoritativeUser)
     else
-      None
+      _checkUserHistoryPerhapsApprove(pageReq)
   }
+
+
+  private def _checkUserHistoryPerhapsApprove(pageReq: PageRequest[_])
+        : Option[Approval] = {
+
+    val (actionsFromIp, peopleFromIp) =
+      pageReq.dao.loadRecentActionExcerpts(
+        fromIp = Some(pageReq.ip), limit = Limit)
+
+    val (actionsByIdentity, peopleForIdty) =
+      pageReq.dao.loadRecentActionExcerpts(
+        byIdentity = Some(pageReq.identity_!.id), limit = Limit)
+
+    // lazy val actionsByGuestsWithSameEmail =
+    // -- or??: lazy val actionsByAnyoneWithSameEmail =
+    //   if (pageReq.emailAddr.isEmpty) Nil
+    //   else pageReq.dao.loadRecentActionExcerpts(byEmail = pageReq.emailAddr)
+
+    val recentActions: List[ViAc] =
+      (actionsFromIp.toList ::: actionsByIdentity.toList)
+         .sortBy(- _.creationDati.getTime).distinct
+
+    val approval: Option[Approval] = for {
+      approvalGivenPosts <- _considerPosts(recentActions)
+      approvalGivenFlags <- _considerFlags(recentActions)
+      approvalGivenRatings <- _considerRatings(recentActions)
+    } yield {
+      if (approvalGivenPosts == Approval.Preliminary ||
+          approvalGivenFlags == Approval.Preliminary ||
+          approvalGivenRatings == Approval.Preliminary) {
+        Approval.Preliminary
+      } else {
+        assert(approvalGivenPosts == Approval.WellBehavedUser)
+        assert(approvalGivenFlags == Approval.WellBehavedUser)
+        assert(approvalGivenRatings == Approval.WellBehavedUser)
+        Approval.WellBehavedUser
+      }
+    }
+
+    approval
+  }
+
+
+  private def _considerPosts(recentActions: List[ViAc]): Option[Approval] = {
+    var manuallyApprovedCount = 0
+    var unreviewedCount = 0
+    var postCount = 0
+
+    for {
+      action <- recentActions if action.isInstanceOf[ViPo]
+      post = action.asInstanceOf[ViPo]
+    } {
+      // If any recent post has been rejected, don't auto approve.
+      if (post.currentVersionRejected)
+        return None
+
+      postCount += 1
+      if (post.someVersionManuallyApproved) manuallyApprovedCount += 1
+      else unreviewedCount += 1
+    }
+
+    // If there are too many outstanding unreviewed comments, don't approve.
+    // (Even if all your comments that've been reviewed have actually been
+    // approved.)
+    if (unreviewedCount >= TooManyUnreviewedComments)
+      return None
+
+    // If all initial comments, plus the moderated one,
+    // have been approved, auto approve.
+    if (manuallyApprovedCount > NumFirstCommentsToApprovePreliminarily)
+      return Some(Approval.Preliminary)
+
+    // Allow all new users to post a few comments, and approve them
+    // preliminarily. I hope that spammers tend to post many comments?
+    if (postCount < NumFirstCommentsToApprovePreliminarily)
+      return Some(Approval.Preliminary)
+
+    // If you've posted more than 1 or 2 comments, and they haven't
+    // been reviewed, don't auto approve any more comments.
+    None
+  }
+
+
+  private def _considerFlags(recentActions: List[ViAc]): Option[Approval] = {
+    for (flag: ViAc <- recentActions if flag.action.isInstanceOf[Flag]) {
+      // If any post has been flagged, don't approve.
+      return None
+    }
+    Some(Approval.WellBehavedUser)
+  }
+
+
+  private def _considerRatings(recentActions: List[ViAc]): Option[Approval] = {
+    // Don't consider ratings at all, for now.
+    Some(Approval.WellBehavedUser)
+  }
+
 
 }
---
+++ test/debiki/AutoApproverSpec.scala
@@ -0,0 +1,220 @@
+/**
+ * Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
+ */
+
+package debiki
+
+import controllers._
+import com.debiki.v0._
+import java.{util => ju}
+import org.specs2.mutable._
+import org.specs2.mock._
+import Prelude._
+import play.api.mvc.Request
+
+
+class AutoApproverSpec extends Specification with Mockito {
+
+  val startDati = new ju.Date(10 * 1000)
+  val TenantId = "tenantid"
+  val Ip = "1.1.1.1"
+  val PageId = "pageid"
+
+  val guestUser = User(
+    id = "-guestid",
+    displayName = "Guest Name",
+    email = "guest.email@.com",
+    emailNotfPrefs = null,
+    country = "",
+    website = "",
+    isAdmin = false,
+    isOwner = false)
+
+  val guestIdty = IdentitySimple(
+    id = guestUser.id drop 1, // drop "-"
+    userId = guestUser.id,
+    name = guestUser.displayName,
+    email = guestUser.email,
+    location = guestUser.country,
+    website = guestUser.website)
+
+  val openidUser = User(
+    id = "openid",
+    displayName = "Oid User Name",
+    email = "oid.email@.com",
+    emailNotfPrefs = null,
+    country = "",
+    website = "",
+    isAdmin = false,
+    isOwner = false)
+
+  val openidIdty = IdentityOpenId(
+    id = "oididtyid",
+    userId = openidUser.id,
+    oidEndpoint = "",
+    oidVersion = "",
+    oidRealm = "",
+    oidClaimedId = "",
+    oidOpLocalId = "",
+    firstName = openidUser.displayName,
+    email = openidUser.email,
+    country = openidUser.country)
+
+  val PlayReq = new Request[Unit] {
+    def uri = "uri"
+    def path = "path"
+    def method = "METHOD"
+    def queryString = Map.empty
+    def headers = null
+    lazy val remoteAddress = Ip
+    def username = None
+    val body = ()
+  }
+
+  val pagePath = PagePath(
+     tenantId = TenantId,
+     folder = "/",
+     pageId = Some(PageId),
+     showId = true,
+     pageSlug = "page-slug")
+
+  def pageReq(user: User, identity: Identity)(dao: TenantDao) =
+    PageRequest[Unit](
+      sid = null,
+      xsrfToken = null,
+      identity = Some(identity),
+      user = Some(user),
+      pageExists = true,
+      pagePath = pagePath,
+      permsOnPage = PermsOnPage.All,
+      dao = dao,
+      request = PlayReq)
+
+  def pageReqOpenId = pageReq(openidUser, openidIdty) _
+  def pageReqGuest = pageReq(guestUser, guestIdty) _
+
+  val quotaConsumers = QuotaConsumers(
+    tenantId = TenantId, ip = Some(Ip), roleId = None)
+
+  val peopleNoLogins =
+    People() + guestIdty + openidIdty + guestUser + openidUser
+
+  val loginId = "101"
+
+  val body =
+    Post(id = "1", parent = "1", ctime = startDati,
+      loginId = loginId, newIp = None, text = "täxt-tåxt",
+      markup = "", approval = None, tyype = PostType.Text,
+      where = None)
+
+  val replyA = body.copy(id = "2", parent = body.id)
+  val replyB = body.copy(id = "3", parent = body.id)
+
+  val (guestLogin, openidLogin) = {
+    val login = Login(id = loginId, ip = Ip, prevLoginId = None,
+       date = startDati, identityId = "?")
+    (login.copy(identityId = guestIdty.id),
+      login.copy(identityId = openidIdty.id))
+  }
+
+  def newDaoMock(actions: List[Post], login: Login) = {
+
+    val viacs: Seq[ViAc] = {
+      val page = Debate("pageid") ++ actions
+      actions map (new ViPo(page, _))
+    }
+
+    val people =
+      if (actions nonEmpty) peopleNoLogins + login
+      else People.None
+
+    val daoMock = mock[TenantDao]
+    daoMock.tenantId returns TenantId
+    daoMock.quotaConsumers returns quotaConsumers
+
+    daoMock.loadRecentActionExcerpts(
+      fromIp = Some(Ip),
+      limit = AutoApprover.Limit)
+       .returns(viacs -> people)
+
+    daoMock.loadRecentActionExcerpts(
+      byIdentity = Some(guestIdty.id),
+      limit = AutoApprover.Limit)
+       .returns(viacs -> people)
+
+    daoMock
+  }
+
+
+  "AutoApprover" can {
+
+    "approve an admin's comments" >> {
+      pending
+    }
+
+
+    "approve a user's first comments preliminarily" >> {
+
+      "the first one" >> {
+        AutoApprover.perhapsApprove(pageReqGuest(newDaoMock(
+          Nil, null))) must_== Some(Approval.Preliminary)
+      }
+
+      "the second" >> {
+        // SHOULD prel approve replyA.
+        AutoApprover.perhapsApprove(pageReqGuest(newDaoMock(
+          List(replyA), guestLogin))) must_== Some(Approval.Preliminary)
+      }
+
+      "but not the third one" >> {
+        // SHOULD prel approve replyA and B.
+        AutoApprover.perhapsApprove(pageReqGuest(newDaoMock(
+          List(replyA, replyB), guestLogin))) must_== None
+      }
+    }
+
+
+    "approve a well behaved user's comments, preliminarily" >> {
+
+      "if many comments have already been approved" >> {
+        pending
+      }
+
+      "unless too many unreviewed comments, from that user" >> {
+        pending
+      }
+
+      "unless too many unreviewed comments, from all users" >> {
+        pending
+      }
+    }
+
+
+    "queue comment for moderation" >> {
+
+      "if any recent comment rejected" >> {
+        pending
+      }
+
+      "if any recent comment flagged" >> {
+        pending
+      }
+    }
+
+
+    "throw Forbidden response" >> {
+
+      "if a spammer has fairly many comments pending moderation" >> {
+        pending
+      }
+
+      "if a well user has terribly many comments pending moderation" >> {
+        pending
+      }
+    }
+
+  }
+
+}
+
+
