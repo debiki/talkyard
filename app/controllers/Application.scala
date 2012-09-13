8ce2e97528e93c7f8e24684812030ea21f6d8916
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-09-13 08:20:33 +0200

    Make _adminPageFile work also when file packaged in JAR.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -239,9 +239,13 @@ object Application extends mvc.Controller {
   /**
    * The file is not secret, but mostly unusable, unless you've logged in.
    */
-  private val _adminPageFile = {
-    val adminPageUrl = this.getClass().getResource("/public/admin/index.html")
-    new jio.File(adminPageUrl.toURI())
+  private val _adminPageFileString: String = {
+    // In prod builds, the file is embedded in a JAR, and accessing it via
+    // an URI causes an IllegalArgumentException: "URI is not hierarchical".
+    // So use a stream instead.
+    val adminPageStream: jio.InputStream =
+      this.getClass().getResourceAsStream("/public/admin/index.html")
+    io.Source.fromInputStream(adminPageStream).mkString("")
   }
 
 
@@ -259,7 +263,7 @@ object Application extends mvc.Controller {
         returnToUrl = request.uri, title = "Login", message = Some(
           "Login with an administrator account to access this page.")))
     else
-      Ok.sendFile(_adminPageFile, inline = true).withCookies(
+      Ok(_adminPageFileString) as HTML withCookies (
           mvc.Cookie(
             DebikiSecurity.AngularJsXsrfCookieName, xsrfOk.value,
             httpOnly = false))
