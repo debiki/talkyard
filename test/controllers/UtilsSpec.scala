c8b11986b1d77eee6feb59efefd77aa7ed0e811e
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-12-12 11:35:00 +0100

    Fix broken test.

--- test/controllers/UtilsSpec.scala
+++ test/controllers/UtilsSpec.scala
@@ -16,17 +16,16 @@ class UtilsSpec extends Specification {
 
     import Utils.parsePathRanges
 
-    val pagePath = PagePath("tenantId", "/folder/", pageId = None,
-      showId = false, pageSlug = "")
+    val baseFolder = "/folder/"
 
     "fallback to defaults" >> {
-      val ranges = parsePathRanges(pagePath, Map.empty)
+      val ranges = parsePathRanges(baseFolder, Map.empty)
       ranges must be_==(PathRanges(folders = Seq("/folder/"), trees = Nil))
     }
 
     "understand &in-folder and &for-folder" >> {
-      val rangesIn = parsePathRanges(pagePath, Map("in-folder" -> Seq("")))
-      val rangesFor = parsePathRanges(pagePath, Map("for-folder" -> Seq("")),
+      val rangesIn = parsePathRanges(baseFolder, Map("in-folder" -> Seq("")))
+      val rangesFor = parsePathRanges(baseFolder, Map("for-folder" -> Seq("")),
          "for")
       val key = PathRanges(folders = Seq("/folder/"), trees = Nil)
       rangesIn must be_==(key)
@@ -34,8 +33,8 @@ class UtilsSpec extends Specification {
     }
 
     "understand &in-tree and &for-tree" >> {
-      val rangesIn = parsePathRanges(pagePath, Map("in-tree" -> Seq("")))
-      val rangesFor = parsePathRanges(pagePath, Map("for-tree" -> Seq("")),
+      val rangesIn = parsePathRanges(baseFolder, Map("in-tree" -> Seq("")))
+      val rangesFor = parsePathRanges(baseFolder, Map("for-tree" -> Seq("")),
          "for")
       val key = PathRanges(folders = Nil, trees = Seq("/folder/"))
       rangesIn must be_==(key)
@@ -43,25 +42,25 @@ class UtilsSpec extends Specification {
     }
 
     "understand &in-folders=f/" >> {
-      val ranges = parsePathRanges(pagePath, Map("in-folders" -> Seq("f/")))
+      val ranges = parsePathRanges(baseFolder, Map("in-folders" -> Seq("f/")))
       ranges must be_==(PathRanges(folders = Seq("/folder/f/"), trees = Nil))
     }
 
     "understand &in-folders=f/,f2/&in-trees=t/,t2/" >> {
-      val ranges = parsePathRanges(pagePath, Map(
+      val ranges = parsePathRanges(baseFolder, Map(
         "in-folders" -> Seq("f/,f2/"), "in-trees" -> Seq("t/,t2/")))
       ranges must be_==(PathRanges(folders = Seq("/folder/f/", "/folder/f2/"),
         trees = Seq("/folder/t/", "/folder/t2/")))
     }
 
     "understand absolute paths: /f/ and /t/" >> {
-      val ranges = parsePathRanges(pagePath, Map(
+      val ranges = parsePathRanges(baseFolder, Map(
         "in-folders" -> Seq("/f/"), "in-trees" -> Seq("/t/")))
       ranges must be_==(PathRanges(folders = Seq("/f/"), trees = Seq("/t/")))
     }
 
     "understand &for-pages=aa,bb" >> {
-      val ranges = parsePathRanges(pagePath, Map("for-pages" -> Seq("aa,bb")))
+      val ranges = parsePathRanges(baseFolder, Map("for-pages" -> Seq("aa,bb")))
       ranges must be_==(PathRanges(pageIds = Seq("aa", "bb")))
     }
   }
