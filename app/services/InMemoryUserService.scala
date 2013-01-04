546fb693610c060dfbc6259e51ba42735155b426
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2013-01-04 12:57:09 +0100

    Use SecureSocial 2.0.8 for Play 2.1, remove workaround
    - Remove workaround (e.g. files copied from SecureSocial and softlinks
      into SecureSocial) from commit 02e3e7ad4, 2012-11-19
    - Instead use my SecureSocial branch for SecureSocial 2.0.8 but fixed to
      work with Play 2.1 and Scala 2.10. (Use commit 1865cda1 from today,
      2012-01-04)
    - Copy a new version of InMemoryUserService from SecureSocial
    - Recreate modules/securesocial softlink

--- app/services/InMemoryUserService.scala
+++ app/services/InMemoryUserService.scala
@@ -16,8 +16,9 @@
  */
 package services
 
-import play.api.Application
-import securesocial.core.{UserServicePlugin, UserId, SocialUser}
+import play.api.{Logger, Application}
+import securesocial.core.{Identity, UserServicePlugin, UserId}
+import securesocial.core.providers.Token
 
 
 /**
@@ -27,13 +28,44 @@ import securesocial.core.{UserServicePlugin, UserId, SocialUser}
  * it stores everything in memory.
  */
 class InMemoryUserService(application: Application) extends UserServicePlugin(application) {
-  private var users = Map[String, SocialUser]()
+  private var users = Map[String, Identity]()
+  private var tokens = Map[String, Token]()
 
   def find(id: UserId) = {
+    if ( Logger.isDebugEnabled ) {
+      Logger.debug("users = %s".format(users))
+    }
     users.get(id.id + id.providerId)
   }
 
-  def save(user: SocialUser) {
+  def findByEmailAndProvider(email: String, providerId: String): Option[Identity] = {
+    if ( Logger.isDebugEnabled ) {
+      Logger.debug("users = %s".format(users))
+    }
+    users.values.find( u => u.email.map( e => e == email && u.id.providerId == providerId).getOrElse(false))
+  }
+
+  def save(user: Identity) {
     users = users + (user.id.id + user.id.providerId -> user)
+  }
+
+  def save(token: Token) {
+    tokens += (token.uuid -> token)
+  }
+
+  def findToken(token: String): Option[Token] = {
+    tokens.get(token)
+  }
+
+  def deleteToken(uuid: String) {
+    tokens -= uuid
+  }
+
+  def deleteTokens() {
+    tokens = Map()
+  }
+
+  def deleteExpiredTokens() {
+    tokens = tokens.filter(!_._2.isExpired)
   }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -20,7 +20,8 @@ object ApplicationBuild extends Build {
     dependsOn(debikiCore, debikiTckDao % "test"))
 
   lazy val secureSocialDeps = Seq(
-    // "com.typesafe" %% "play-plugins-util" % "2.0.1", // notTransitive(),
+    // "com.typesafe" %% "play-plugins-util" % "2.0.1", // notTransitive(), — not available yet for Scala 2.10? (as of December 19, 2012 - and certainly not in October 2012.)
+    // "com.typesafe" %% "play-plugins-mailer" % "2.1-RC1", is this available anywhere?
     "org.mindrot" % "jbcrypt" % "0.3m")
 
   lazy val secureSocial =
@@ -31,6 +32,7 @@ object ApplicationBuild extends Build {
         "jBCrypt Repository" at "http://repo1.maven.org/maven2/org/",
         "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/")
     )
+
 
   val appDependencies = Seq(
     "com.amazonaws" % "aws-java-sdk" % "1.3.4",
