e1b79721b317cdae764d12d20ddf6161a2dbc000
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2013-01-05 12:23:54 +0100

    Remove broken unused SecureSocial login function.

--- app/controllers/AppLogin.scala
+++ app/controllers/AppLogin.scala
@@ -58,39 +58,14 @@ object AppLogin extends mvc.Controller {
         returnToUrl = returnToUrl)
     }
 
-    // Not async? Will this block a thread??
-    def _loginWithOAuth(provider: String): Result = {
-      securesocial.core.ProviderRegistry.get(provider) match {
-        case Some(p) =>
-          try {
-            p.authenticate().fold(
-              result => result,
-              user => {
-                Logger.debug("User logged in: [" + user + "]")
-                Redirect(returnToUrl) /* .withSession {
-                  session +
-                   (SecureSocial.UserKey -> user.id.id) +
-                   (SecureSocial.ProviderKey -> user.id.providerId) -
-                   SecureSocial.OriginalUrlKey
-                }*/
-              })
-          } catch {
-            case ex: securesocial.core.AccessDeniedException =>
-              Logger.warn("User declined access using provider " + provider)
-              throwForbidden("DwE93Z4", "You declined access?")
-          }
-        case None =>
-          throwForbidden("DwE09PJ3", "Unsupported provider: "+ provider)
-      }
-    }
-
     provider match {
       case "google" =>
         _loginWithOpenId(IdentityOpenId.ProviderIdentifier.Google)
       case "yahoo" =>
         _loginWithOpenId(IdentityOpenId.ProviderIdentifier.Yahoo)
       case x =>
-        _loginWithOAuth(provider = x)
+        unimplemented("Logging in with SecureSocial from here")
+        // Or forward to AppLoginSecureSocial.handleAuth in some manner?
     }
   }
 
