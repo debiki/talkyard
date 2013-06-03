ebb6e7b84a351ace1c088fced56b835461ae3aac
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2013-06-03 09:35:25 +0200

    License Scala files under the AGPL license
    Also add a few comments.

--- app/controllers/AppEdit.scala
+++ app/controllers/AppEdit.scala
@@ -1,5 +1,18 @@
 /**
- * Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
+ * Copyright (C) 2012-2013 Kaj Magnus Lindberg (born 1979)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package controllers
@@ -21,7 +34,8 @@ import v0.{PostActionPayload => PAP}
 import Utils.{OkHtml, Passhasher, parseIntOrThrowBadReq}
 
 
-/**
+/** Edits pages. And lazily saves new unsaved pages "created" by AppCreatePage.
+  *
  * SECURITY BUG I think it's possible to use edit GET/POST requests
  * to access and *read* hidden pages. I don't think I do any access control
  * when sending the current markup source back to the browser? Only when
