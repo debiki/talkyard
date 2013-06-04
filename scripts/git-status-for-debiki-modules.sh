11019614a17aa3b8b907cc7546583aafc0749e67
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2013-06-04 08:29:29 +0200

    Fix broken git-status script.

--- scripts/git-status-for-debiki-modules.sh
+++ scripts/git-status-for-debiki-modules.sh
@@ -1,1 +1,5 @@
-pushd . ; gis ; cd modules/debiki-core/ ; gis ; cd ../debiki-dao-pgsql ; gis ; cd ../debiki-tck-dao ; gis ; popd
+#!/bin/bash
+# Runs `git status` in all Debiki's modules.
+
+pushd . ; git status ; cd modules/debiki-core/ ; git status ; cd ../debiki-dao-pgsql ; git status ; cd ../debiki-tck-dao ; git status ; popd
+
