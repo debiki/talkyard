02058e1ed6b9dafe4d5803d2d6cb872fb04d860e
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2013-06-10 18:03:39 +0200

    Move YoxView away from this repo
    It's www.debiki.com specific. Place it in my debiki-site-www.debiki.com repo
    instead, and, for now only, add a softlink to it.

--- public/tmp/yoxview/yoxview-nojquery.js
+++
@@ -1,5 +0,0 @@
-﻿jQuery(document).ready(function($){
-    // Uncomment the following line if you use an additional Javascript library (likr Prototype, for example) in the same page as YoxView:
-    // jQuery.noConflict();
-    $(".yoxview").yoxview();
-});
