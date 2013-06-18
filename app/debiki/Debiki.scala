d7c6773de30af8a65e0a921afa82187f6cce087d
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2013-06-18 07:46:40 +0200

    Use PostgreSQL data source, not BoneCP
    There's some problem with BoneCP's connection pool, resulting in:
      java.sql.SQLException: Timed out waiting for a free available connection.

--- app/debiki/Debiki.scala
+++ app/debiki/Debiki.scala
@@ -31,14 +31,15 @@ object Debiki extends Debiki
 class Debiki {
 
   private val dbDaoFactory = new RelDbDaoFactory({
-    val dataSourceName = if (Play.isTest) "test" else "default"
-    val dataSource = p.db.DB.getDataSource(dataSourceName)
+    //val dataSourceName = if (Play.isTest) "test" else "default"
+    //val dataSource = p.db.DB.getDataSource(dataSourceName)
+    val dataSource = getPostgreSqlDataSource()
     val db = new RelDb(dataSource)
 
     // Log which database we've connected to.
-    val boneDataSource = dataSource.asInstanceOf[com.jolbox.bonecp.BoneCPDataSource]
-    p.Logger.info(o"""Connected to database:
-      ${boneDataSource.getJdbcUrl} as user ${boneDataSource.getUsername}.""")
+    //val boneDataSource = dataSource.asInstanceOf[com.jolbox.bonecp.BoneCPDataSource]
+    //p.Logger.info(o"""Connected to database:
+    //  ${boneDataSource.getJdbcUrl} as user ${boneDataSource.getUsername}.""")
 
     db
   })
@@ -74,6 +75,53 @@ class Debiki {
 
   def sendEmail(email: Email, websiteId: String) {
     mailerActorRef ! (email, websiteId)
+  }
+
+
+  // Play's BoneCP v.0.7.1 is broken and unusable, and SBT refuses to use version 0.8 RC1
+  // which supposedly has fixed the problem. Use Postgres connection pool instead.
+  // The problem is:
+  //    java.sql.SQLException: Timed out waiting for a free available connection.
+  //    at com.jolbox.bonecp.BoneCP.getConnection(BoneCP.java:503)
+  //                                  ~[com.jolbox.bonecp-0.7.1.RELEASE.jar:0.7.1.RELEASE
+  // See: http://stackoverflow.com/questions/15480506/
+  //                        heroku-play-bonecp-connection-issues/15500442#15500442
+  private def getPostgreSqlDataSource(): javax.sql.DataSource = {
+
+    def configPrefix = if (Play.isTest) "test." else ""
+
+    def configStr(path: String) =
+      Play.configuration.getString(configPrefix + path) getOrElse
+        runErr("DwE93KI2", "Config value missing: "+ path)
+
+    // I've hardcoded credentials to the test database here, so that it
+    // cannot possibly happen, that you accidentally connect to the prod
+    // database. (You'll never name the prod schema "debiki_test_0_0_2_empty",
+    // with "auto-dropped" as password?)
+    def user =
+      if (Play.isTest) "debiki_test_0_0_2_empty"
+      else configStr("debiki.pgsql.user")
+
+    def password =
+      if (Play.isTest) "auto-dropped"
+      else configStr("debiki.pgsql.password")
+
+    val ds = new org.postgresql.ds.PGPoolingDataSource()
+    ds.setDataSourceName("DebikiPostgreConnCache"+ math.random)
+    val server = configStr("debiki.pgsql.server")
+    val port = configStr("debiki.pgsql.port").toInt
+    val dbName = configStr("debiki.pgsql.database")
+    ds.setServerName(server)
+    ds.setPortNumber(port)
+    ds.setDatabaseName(dbName)
+    ds.setUser(user)
+    ds.setPassword(password)
+    ds.setInitialConnections(2)
+    ds.setMaxConnections(10)
+    ds.setPrepareThreshold(3)
+
+    play.Logger.info(s"""Connected to database: $server:$port/$dbName as $user""")
+    ds
   }
 
 }
