36369acd75498cd790732a4d537bb773fb464db7
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2013-11-30 05:00:43 +0100

    Break out saveReply() function from AppReply.handleForm()
    And remove _showHtmlResultPage(), which is used for static HTML only.

--- app/controllers/AppReply.scala
+++ app/controllers/AppReply.scala
@@ -40,57 +40,44 @@ object AppReply extends mvc.Controller {
 
   def handleForm(pathIn: PagePath, postId: ActionId)
         = PagePostAction(MaxPostSize)(pathIn) {
-      pageReqNoMeOnPage: PagePostRequest =>
+      pageReq: PagePostRequest =>
 
     import Utils.ValidationImplicits._
+
+    val text = pageReq.getEmptyAsNone("dw-fi-reply-text") getOrElse
+      throwBadReq("DwE93k21", "Empty reply")
+    val whereOpt = pageReq.getEmptyAsNone("dw-fi-reply-where")
+
+    val json = saveReply(pageReq, replyTo = postId, text, whereOpt)
+    OkSafeJson(json)
+  }
+
+
+  def saveReply(pageReqNoMeOnPage: PageRequest[_], replyTo: PostId, text: String,
+        whereOpt: Option[String] = None) = {
+
+    val postIdToReplyTo = replyTo
 
     val pageReq = pageReqNoMeOnPage.copyWithMeOnPage_!
     if (pageReq.oldPageVersion.isDefined)
       throwBadReq("DwE72XS8", "Can only reply to latest page version")
 
-    if (pageReq.page_!.getPost(postId) isEmpty)
-      throwBadReq("DwEe8HD36", s"Cannot reply to post `$postId'; it does not exist")
-
-    val text = pageReq.getEmptyAsNone("dw-fi-reply-text") getOrElse
-      throwBadReq("DwE93k21", "Empty reply")
-    val whereOpt = pageReq.getEmptyAsNone("dw-fi-reply-where")
+    if (pageReq.page_!.getPost(postIdToReplyTo) isEmpty)
+      throwBadReq("DwEe8HD36", s"Cannot reply to post `$postIdToReplyTo'; it does not exist")
 
     val approval = AutoApprover.perhapsApprove(pageReq)
 
     val postNoId = PostActionDto(id = PageParts.UnassignedId, postId = PageParts.UnassignedId,
       creationDati = pageReq.ctime, loginId = pageReq.loginId_!, userId = pageReq.user_!.id,
       newIp = pageReq.newIp, payload = PAP.CreatePost(
-        parentPostId = postId, text = text, markup = Markup.DefaultForComments.id,
+        parentPostId = postIdToReplyTo, text = text, markup = Markup.DefaultForComments.id,
         where = whereOpt, approval = approval))
 
     val (pageWithNewPost, List(postWithId: PostActionDto[PAP.CreatePost])) =
       pageReq.dao.savePageActionsGenNotfs(pageReq, postNoId::Nil)
 
-    if (pageReq.isAjax) {
-      val patchSpec = TreePatchSpec(postWithId.id, wholeTree = true)
-      OkSafeJson(
-        BrowserPagePatcher(pageReq).jsonForTrees(pageWithNewPost.parts, patchSpec))
-    }
-    else {
-      _showHtmlResultPage(pageReq, postWithId)
-    }
-  }
-
-
-  private def _showHtmlResultPage(
-        pageReq: PageRequest[_], post: PostActionDto[PAP.CreatePost]): PlainResult = {
-    val nextPageUrl =
-      Utils.queryStringAndHashToView(pageReq.pageRoot, pageReq.oldPageVersion,
-         post.payload.approval.map(_ => post.id),
-         // Write a '?' even if query string is empty, so '?reply' is removed.
-         forceQuery = true)
-    OkHtmlBody(
-      if (post.payload.approval.isDefined)
-        <p>Comment saved.</p>
-        <p><a href={nextPageUrl}>Okay, let me view it</a></p>
-      else
-        <p>Comment awaiting moderation.</p>
-        <p><a href={nextPageUrl}>Okay, return to page</a></p>)
+    val patchSpec = TreePatchSpec(postWithId.id, wholeTree = true)
+    BrowserPagePatcher(pageReq).jsonForTrees(pageWithNewPost.parts, patchSpec)
   }
 
 }
