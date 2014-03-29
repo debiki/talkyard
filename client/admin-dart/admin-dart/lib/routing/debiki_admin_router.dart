7733b7ac0ef043a06beef76867b0d371fc3989c5
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2014-03-29 08:54:53 +0100

    Move-rename AngularDart html templates
    Move whole site stuff to site/ subdir.

--- client/admin-dart/admin-dart/lib/routing/debiki_admin_router.dart
+++ client/admin-dart/admin-dart/lib/routing/debiki_admin_router.dart
@@ -10,19 +10,19 @@ class DebikiAdminRouteInitializer implements RouteInitializer {
       ..addRoute(
           name: 'allRecentTopics',
           path: '/all-recent-topics',
-          enter: view('view/all-recent-topics.html'))
+          enter: view('view/site/all_recent_topics.html'))
       ..addRoute(
           name: 'allRecentComments',
           path: '/all-recent-comments',
-          enter: view('view/all-recent-comments.html'))
+          enter: view('view/site/all_recent_comments.html'))
       ..addRoute(
           name: 'siteWideSettings',
           path: '/site-wide-settings',
-          enter: view('view/site-wide-settings.html'))
+          enter: view('view/site/site_wide_settings.html'))
       ..addRoute(
           name: 'siteWideSpecialContent',
           path: '/site-wide-special-content',
-          enter: view('view/site/site_special_content.html'))
+          enter: view('view/site/site_wide_special_content.html'))
       ..addRoute(
           name: 'pages',
           path: '/pages/',
@@ -30,19 +30,19 @@ class DebikiAdminRouteInitializer implements RouteInitializer {
             ..addRoute(
                 name: 'pagesList',
                 path: 'list',
-                enter: view('view/pages/pages-list.html'))
+                enter: view('view/pages/pages_list.html'))
             ..addRoute(
                 name: 'pagesComments',
                 path: 'recent-comments',
-                enter: view('view/pages/recent-comments.html'))
+                enter: view('view/pages/pages_recent_comments.html'))
             ..addRoute(
                 name: 'pagesSettings',
                 path: 'settings',
-                enter: view('view/pages/settings.html'))
+                enter: view('view/pages/pages_settings.html'))
             ..addRoute(
                 name: 'pagesDashboard',
                 path: '',
-                enter: view('view/pages/dashboard.html')))
+                enter: view('view/pages/pages_dashboard.html')))
       ..addRoute(
           name: 'blog',
           path: '/blog/:blogId/',
@@ -50,19 +50,19 @@ class DebikiAdminRouteInitializer implements RouteInitializer {
             ..addRoute(
                 name: 'blogPosts',
                 path: 'recent-posts',
-                enter: view('view/blog/recent-posts.html'))
+                enter: view('view/blog/blog_recent_posts.html'))
             ..addRoute(
                 name: 'blogComments',
                 path: 'recent-comments',
-                enter: view('view/blog/recent-comments.html'))
+                enter: view('view/blog/blog_recent_comments.html'))
             ..addRoute(
                 name: 'blogSettings',
                 path: 'settings',
-                enter: view('view/blog/settings.html'))
+                enter: view('view/blog/blog_settings.html'))
             ..addRoute(
                 name: 'blogDashboard',
                 path: '',
-                enter: view('view/blog/dashboard.html')))
+                enter: view('view/blog/blog_dashboard.html')))
       ..addRoute(
           name: 'forum',
           path: '/fourm/:forumId/',
@@ -70,23 +70,23 @@ class DebikiAdminRouteInitializer implements RouteInitializer {
             ..addRoute(
                 name: 'forumTopics',
                 path: 'recent-topics',
-                enter: view('view/forum/recent-topics.html'))
+                enter: view('view/forum/forum_recent_topics.html'))
             ..addRoute(
                 name: 'forumComments',
                 path: 'recent-comments',
-                enter: view('view/forum/recent-comments.html'))
+                enter: view('view/forum/forum_recent_comments.html'))
             ..addRoute(
                 name: 'forumSettings',
                 path: 'settings',
-                enter: view('view/forum/settings.html'))
+                enter: view('view/forum/forum_settings.html'))
             ..addRoute(
                 name: 'forumSpecialContent',
                 path: 'special-content',
-                enter: view('view/forum/special_content.html'))
+                enter: view('view/forum/forum_special_content.html'))
             ..addRoute(
                 name: 'forumDashboard',
                 path: '',
-                enter: view('view/forum/dashboard.html')))
+                enter: view('view/forum/forum_dashboard.html')))
       ..addRoute(
           name: 'embeddedComments',
           path: '/embedded-comments/',
@@ -94,19 +94,19 @@ class DebikiAdminRouteInitializer implements RouteInitializer {
             ..addRoute(
                 name: 'embeddedCommentsTopics',
                 path: 'recent-topics',
-                enter: view('view/embedded-comments/recent-topics.html'))
+                enter: view('view/embedded-comments/embedded_recent_topics.html'))
             ..addRoute(
                 name: 'embeddedCommentsComments',
                 path: 'recent-comments',
-                enter: view('view/embedded-comments/recent-comments.html'))
+                enter: view('view/embedded-comments/embedded_recent_comments.html'))
             ..addRoute(
                 name: 'embeddedCommentsSettings',
                 path: 'settings',
-                enter: view('view/embedded-comments/settings.html')))
+                enter: view('view/embedded-comments/embedded_settings.html')))
       ..addRoute(
           name: 'mainDashboard',
           path: '/',
-          enter: view('view/main-dashboard.html'))
+          enter: view('view/site/site_dashboard.html'))
       ..addRoute(
           name: 'default',
           defaultRoute: true,
