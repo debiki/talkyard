0e70fd6e7b2479e694209c6bcf46fb53558e2a14
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2014-04-29 05:44:46 +0200

    Add test cases for PagePath and "/-pageid-slug" page names.

--- modules/debiki-core/src/test/scala/com/debiki/core/PagePathTest.scala
+++ modules/debiki-core/src/test/scala/com/debiki/core/PagePathTest.scala
@@ -26,7 +26,101 @@ import com.debiki.core.PostActionDto._
 
 class PagePathTest extends FreeSpec with MustMatchers {
 
+  def parse(urlPath: String): PagePath = PagePath.fromUrlPath("tenantId", urlPath) match {
+    case PagePath.Parsed.Bad(error) =>
+      fail(s"Error parsing path: $urlPath, error: $error")
+    case PagePath.Parsed.Corrected(correctedPath) =>
+      fail(s"Error parsing path: `$urlPath', wants to correct it to: $correctedPath")
+    case PagePath.Parsed.Good(pagePath) =>
+      pagePath
+  }
+
   "PagePath can" - {
+
+    "parse '/'" in {
+      val pagePath = parse("/")
+      pagePath.folder mustBe "/"
+      pagePath.pageSlug mustBe ""
+      pagePath.pageId mustBe None
+    }
+
+    "parse '/folder/'" in {
+      val pagePath = parse("/folder/")
+      pagePath.folder mustBe "/folder/"
+      pagePath.pageSlug mustBe ""
+      pagePath.pageId mustBe None
+    }
+
+    "parse '/so/many/foldes/'" in {
+      val pagePath = parse("/so/many/folders/")
+      pagePath.folder mustBe "/so/many/folders/"
+      pagePath.pageSlug mustBe ""
+      pagePath.pageId mustBe None
+    }
+
+    "parse '/slug'" in {
+      val pagePath = parse("/slug")
+      pagePath.folder mustBe "/"
+      pagePath.pageSlug mustBe "slug"
+      pagePath.pageId mustBe None
+    }
+
+    "parse '/folder/slug'" in {
+      val pagePath = parse("/folder/slug")
+      pagePath.folder mustBe "/folder/"
+      pagePath.pageSlug mustBe "slug"
+      pagePath.pageId mustBe None
+    }
+
+    "parse '/so/many/folders/slug'" in {
+      val pagePath = parse("/so/many/folders/slug")
+      pagePath.folder mustBe "/so/many/folders/"
+      pagePath.pageSlug mustBe "slug"
+      pagePath.pageId mustBe None
+    }
+
+    "parse '/-pageId'" in {
+      val pagePath = parse("/-pageId")
+      pagePath.folder mustBe "/"
+      pagePath.pageSlug mustBe ""
+      pagePath.pageId mustBe Some("pageId")
+    }
+
+    "parse '/folder/-pageId'" in {
+      val pagePath = parse("/folder/-pageId")
+      pagePath.folder mustBe "/folder/"
+      pagePath.pageSlug mustBe ""
+      pagePath.pageId mustBe Some("pageId")
+    }
+
+    "parse '/so/many/folders/-pageId'" in {
+      val pagePath = parse("/so/many/folders/-pageId")
+      pagePath.folder mustBe "/so/many/folders/"
+      pagePath.pageSlug mustBe ""
+      pagePath.pageId mustBe Some("pageId")
+    }
+
+    "parse '/-pageId-slug'" in {
+      val pagePath = parse("/-pageId-slug")
+      pagePath.folder mustBe "/"
+      pagePath.pageSlug mustBe "slug"
+      pagePath.pageId mustBe Some("pageId")
+    }
+
+    "parse '/folder/-pageId-slug'" in {
+      val pagePath = parse("/folder/-pageId-slug")
+      pagePath.folder mustBe "/folder/"
+      pagePath.pageSlug mustBe "slug"
+      pagePath.pageId mustBe Some("pageId")
+    }
+
+    "parse '/so/many/folders/-pageId-slug'" in {
+      val pagePath = parse("/so/many/folders/-pageId-slug")
+      pagePath.folder mustBe "/so/many/folders/"
+      pagePath.pageSlug mustBe "slug"
+      pagePath.pageId mustBe Some("pageId")
+    }
+
 
     "find its parent folder" in {
       val rootFldr: PagePath = PagePath(tenantId = "tenantId", folder = "/",
