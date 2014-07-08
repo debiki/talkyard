e9e0a60ec1642100c62f01ae346d136982120a23
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2014-07-08 17:45:48 +0200

    Add more read-posts test cases.

--- modules/debiki-tck-dao/src/main/scala/com/debiki/tck/dao/specs/PostsReadStatsSpec.scala
+++ modules/debiki-tck-dao/src/main/scala/com/debiki/tck/dao/specs/PostsReadStatsSpec.scala
@@ -40,10 +40,12 @@ class PostsReadStatsSpec(daoFactory: DbDaoFactory) extends DbDaoSpec(daoFactory)
     val PageText = "The page text"
     var post2: Post = null
     var post3: Post = null
+    var post4: Post = null
     var passwordIdentity: PasswordIdentity = null
     var passwordRole: User = null
     var passwordLoginGrant: LoginGrant = null
     var guestLoginGrant: LoginGrant = null
+    var guestLoginGrantOtherIp: LoginGrant = null
     var guestUser: User = null
     val GuestIp = "0.0.0.1"
     val GuestIp2 = "0.0.0.2"
@@ -63,12 +65,17 @@ class PostsReadStatsSpec(daoFactory: DbDaoFactory) extends DbDaoSpec(daoFactory)
       post2 = page.parts.getPost(tmpPost2.id) getOrElse fail("Comment not found")
     }
 
-    "login as guest, add comment #3" in {
+    "login as guest, add comment #3 and #4" in {
       guestLoginGrant = siteUtils.loginAsGuest(name = "Test Guest", ip = GuestIp)
       guestUser = guestLoginGrant.user
+
       val (tmpPage, tmpPost3) = siteUtils.addComment(guestLoginGrant, page, "Guest comment")
       page = siteUtils.review(passwordLoginGrant, tmpPage, tmpPost3.id, Approval.AuthoritativeUser)
       post3 = page.parts.getPost(tmpPost3.id) getOrElse fail("Comment not found")
+
+      val (tmpPage2, tmpPost4) = siteUtils.addComment(guestLoginGrant, page, "Guest comment #4")
+      page = siteUtils.review(passwordLoginGrant, tmpPage2, tmpPost4.id, Approval.AuthoritativeUser)
+      post4 = page.parts.getPost(tmpPost4.id) getOrElse fail("Comment not found")
     }
 
     "find no posts read stats, for non-existing page" in {
@@ -81,69 +88,117 @@ class PostsReadStatsSpec(daoFactory: DbDaoFactory) extends DbDaoSpec(daoFactory)
         PostsReadStats(page.id, Map.empty, Map.empty)
     }
 
-    "add some post read stats" in {
-      val (tmpPage, dummyVote) = siteUtils.vote(guestLoginGrant, page, post2.id, PAP.VoteLike)
-      page = tmpPage
-      siteUtils.dao.updatePostsReadStats(page.id, Set(PageParts.BodyId, post2.id), dummyVote)
+    "vote-read posts #1 and #2 as a guest" - {
+      "like post #2" in {
+        val (tmpPage, vote) = siteUtils.vote(guestLoginGrant, page, post2.id, PAP.VoteLike)
+        page = tmpPage
+        siteUtils.dao.updatePostsReadStats(page.id, Set(PageParts.BodyId, post2.id), vote)
+      }
+
+      "find posts read stats" in {
+        val stats = siteUtils.dao.loadPostsReadStats(page.id)
+        stats mustBe PostsReadStats(
+          page.id,
+          Map(
+            PageParts.BodyId -> Set(GuestIp),
+            post2.id -> Set(GuestIp)),
+          Map.empty)
+      }
     }
 
-    "find posts read stats" in {
-      val stats = siteUtils.dao.loadPostsReadStats(page.id)
-      stats mustBe PostsReadStats(
-        page.id,
-        Map(
-          PageParts.BodyId -> Set(GuestIp),
-          post2.id -> Set(GuestIp)),
-        Map.empty)
+    "vote-read post #1, #2, #3 as guest, not fail on duplicate ip inserts" - {
+      "like post #3" in {
+        val (tmpPage, vote) = siteUtils.vote(guestLoginGrant, page, post3.id, PAP.VoteLike)
+        page = tmpPage
+        siteUtils.dao.updatePostsReadStats(
+          page.id, Set(PageParts.BodyId, post2.id, post3.id), vote)
+      }
+
+      "find posts read stats" in {
+        val stats = siteUtils.dao.loadPostsReadStats(page.id)
+        stats mustBe PostsReadStats(
+          page.id,
+          Map(
+            PageParts.BodyId -> Set(GuestIp),
+            post2.id -> Set(GuestIp),
+            post3.id -> Set(GuestIp)),
+          Map.empty)
+      }
     }
 
-    "add even more post read stats" in {
-      val (tmpPage, dummyVote) = siteUtils.vote(passwordLoginGrant, page, post2.id, PAP.VoteLike)
-      page = tmpPage
-      siteUtils.dao.updatePostsReadStats(
-        page.id, Set(PageParts.BodyId, post2.id), dummyVote)
+    "vote-read post #1, #2, #3, #4 as same guest another ip, not fail on duplicate guest ids" - {
+      "login as guest from other ip" in {
+        guestLoginGrantOtherIp =
+          siteUtils.loginAsGuest(name = guestLoginGrant.displayName, ip = GuestIp2)
+      }
+
+      "like post #4" in {
+        val (tmpPage, vote) = siteUtils.vote(guestLoginGrantOtherIp, page, post4.id, PAP.VoteLike)
+        page = tmpPage
+        siteUtils.dao.updatePostsReadStats(
+          page.id, Set(PageParts.BodyId, post2.id, post3.id, post4.id), vote)
+      }
+
+      "find posts read stats" in {
+        val stats = siteUtils.dao.loadPostsReadStats(page.id)
+        stats mustBe PostsReadStats(
+          page.id,
+          Map(
+            PageParts.BodyId -> Set(GuestIp), // these posts weren't read again, same guest id
+            post2.id -> Set(GuestIp),         //
+            post3.id -> Set(GuestIp),         //
+            post4.id -> Set(GuestIp2)),  // same guest but another ip
+          Map.empty)
+      }
     }
 
-    "find more posts read stats" in {
-      val stats = siteUtils.dao.loadPostsReadStats(page.id)
-      stats mustBe PostsReadStats(
-        page.id,
-        Map(
-          PageParts.BodyId -> Set(GuestIp),
-          post2.id -> Set(GuestIp)),
-        Map(
-          PageParts.BodyId -> Set(passwordRole.id),
-          post2.id -> Set(passwordRole.id)))
+    "vote-read posts #1 and #2 as password user" - {
+      "add even more post read stats" in {
+        val (tmpPage, vote) = siteUtils.vote(passwordLoginGrant, page, post2.id, PAP.VoteLike)
+        page = tmpPage
+        siteUtils.dao.updatePostsReadStats(
+          page.id, Set(PageParts.BodyId, post2.id), vote)
+      }
+
+      "find posts read stats" in {
+        val stats = siteUtils.dao.loadPostsReadStats(page.id)
+        stats mustBe PostsReadStats(
+          page.id,
+          Map(
+            PageParts.BodyId -> Set(GuestIp),
+            post2.id -> Set(GuestIp),
+            post3.id -> Set(GuestIp),
+            post4.id -> Set(GuestIp2)),
+          Map(
+            PageParts.BodyId -> Set(passwordRole.id),
+            post2.id -> Set(passwordRole.id)))
+      }
     }
 
-    "not fail on duplicated ip inserts" in {
-      pending
-    }
+    "vote-read posts #1, #2, #3 as password user, not fail on dupl role id" - {
+      "like comment #3" in {
+        // Now the password user votes again, and attempts to update the posts-read-stats for
+        // posts 1 and 2 again.
+        val (tmpPage, vote) = siteUtils.vote(passwordLoginGrant, page, post3.id, PAP.VoteLike)
+        page = tmpPage
+        siteUtils.dao.updatePostsReadStats(
+          page.id, Set(PageParts.BodyId, post2.id, post3.id), vote)
+      }
 
-    "not fail on duplicated guest id inserts" in {
-      pending
-    }
-
-    "not fail on duplicated role inserts" in {
-      // Now the password user votes again, and attempts to update the posts-read-stats for
-      // posts 1 and 2 again.
-      val (tmpPage, dummyVote) = siteUtils.vote(passwordLoginGrant, page, post3.id, PAP.VoteLike)
-      page = tmpPage
-      siteUtils.dao.updatePostsReadStats(
-        page.id, Set(PageParts.BodyId, post2.id, post3.id), dummyVote)
-    }
-
-    "find even more posts read stats" in {
-      val stats = siteUtils.dao.loadPostsReadStats(page.id)
-      stats mustBe PostsReadStats(
-        page.id,
-        Map(
-          PageParts.BodyId -> Set(GuestIp),
-          post2.id -> Set(GuestIp)),
-        Map(
-          PageParts.BodyId -> Set(passwordRole.id),
-          post2.id -> Set(passwordRole.id),
-          post3.id -> Set(passwordRole.id)))
+      "find posts read stats" in {
+        val stats = siteUtils.dao.loadPostsReadStats(page.id)
+        stats mustBe PostsReadStats(
+          page.id,
+          Map(
+            PageParts.BodyId -> Set(GuestIp),
+            post2.id -> Set(GuestIp),
+            post3.id -> Set(GuestIp),
+            post4.id -> Set(GuestIp2)),
+          Map(
+            PageParts.BodyId -> Set(passwordRole.id),
+            post2.id -> Set(passwordRole.id),
+            post3.id -> Set(passwordRole.id)))
+      }
     }
   }
 
