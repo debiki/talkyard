b3ef797570bc675957568a03b180df6711b48b9c
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2015-01-08 14:32:28 +0100

    Delete old unused test files.

--- scripts-client/e2e-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE_DIR=`dirname $0`
-
-java -jar "$BASE_DIR/../test-client/lib/jstestdriver/JsTestDriver.jar" \
-     --config "$BASE_DIR/../conf-client/jsTestDriver-scenario.conf" \
-     --basePath "$BASE_DIR/.." \
-     --tests all --reset
