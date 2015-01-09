78a64244812994d07c0555a8e99957854cc2a88b
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2015-01-09 07:50:34 +0100

    Switch to correct user before running `gulp watch` in container.

--- docker-start-dev-gulp.sh
+++ docker-start-dev-gulp.sh
@@ -17,13 +17,21 @@ echo '|   npm install                                           |'
 echo '|   GIT_WORK_TREE=/opt/debiki bower --allow-root install  |'
 echo '`--------------------------------------------------------'"'"
 
+# About the Bash command below:
+# 1. `useradd ...` adds a user with the same id as the directory owner (i.e. you), named 'owner'.
+# 2. `ls -adn | cut -f 3 -d ' '` finds the user id of the above-mentioned directory owner.
+# 3. `su owner` drops into a shell as 'owner'.
+# Result: When you inside Docker create files via `npm install` or `gulp watch`, these
+# files will be owned by the directory owner (i.e. you). Otherwise, from outside
+# the container, they'd be onwed by root.
 docker run \
   --rm \
   -it \
   --name debiki-dev-gulp \
   -v="`pwd`/../:/opt/debiki/" \
   debiki-dev-gulp:v0 \
-  /bin/bash
+  /bin/bash -c \
+    'useradd -u `ls -adn | cut -f 3 -d " "` owner ; su owner'
 
 
 # vim: fdm=marker et ts=2 sw=2 tw=0 list
