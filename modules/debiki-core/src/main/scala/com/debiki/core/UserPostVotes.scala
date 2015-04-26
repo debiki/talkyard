2e63a47e94281ca56c1116e7ac5d52cb4a568e4b
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2015-04-26 07:20:12 +0200

    Rename UserPostVotes file.

--- modules/debiki-core/src/main/scala/com/debiki/core/UserPostVotes.scala
+++ modules/debiki-core/src/main/scala/com/debiki/core/UserPostVotes.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012-2013 Kaj Magnus Lindberg (born 1979)
+ * Copyright (C) 2014 Kaj Magnus Lindberg (born 1979)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
