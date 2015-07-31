943b0c8b1d4b2c547ab6dd296a07d61b05e1aab8
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2015-07-31 02:26:15 +0200

    Upgrade to React 0.13.3. Requires Nashorn 8u60 > build b25
    There are bugs in earlier Nashorn versions that result in an
    ArrayIndexOutOfBounds exception.

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,7 @@ var debikiJavascriptFiles = [
       'bower_components/moment/min/moment.min.js',
       'bower_components/eventemitter2/lib/eventemitter2.js',
       'bower_components/react-bootstrap/react-bootstrap.js',
-      'bower_components/react-router/dist/react-router.js',
+      'bower_components/react-router/build/umd/ReactRouter.js',
       'bower_components/Caret.js/dist/jquery.caret.js',
       'bower_components/jquery.atwho/dist/js/jquery.atwho.js',
       'bower_components/nicescroll/jquery.nicescroll.js',
@@ -235,7 +235,7 @@ function compileServerSideTypescript() {
   var javascriptStream = gulp.src([
         'bower_components/react/react-with-addons.js',
         'bower_components/react-bootstrap/react-bootstrap.js',
-        'bower_components/react-router/dist/react-router.js',
+        'bower_components/react-router/build/umd/ReactRouter.js',
         'bower_components/markdown-it/dist/markdown-it.js',
         'bower_components/lodash/dist/lodash.js',
         'client/third-party/html-css-sanitizer-bundle.js',
--- scripts/run-dist.sh
+++ scripts/run-dist.sh
@@ -5,7 +5,7 @@ pushd .
 cd target/universal
 unzip -q debiki-server-1.0-SNAPSHOT.zip
 cd debiki-server-1.0-SNAPSHOT
-bin/debiki-server -Dconfig.file=../../../../conf/debiki.conf -Dhttps.port=9443
+JAVA_HOME=~/Downloads/jdk1.8.0_60 bin/debiki-server -Dconfig.file=../../../../conf/debiki.conf -Dhttps.port=9443
 popd
 
 # vim: list
