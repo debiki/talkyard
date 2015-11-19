f1d7b61d4e2b8c162979e8f2c7d913949fc29ab3
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2015-11-19 05:51:59 +0100

    Fix bad-category-id error message bug.

--- client/app/Server.ts
+++ client/app/Server.ts
@@ -479,6 +479,12 @@ export function listUsernames(prefix: string, doneCallback: (usernames: string[]
 //
 export function loadDraftAndGuidelines(writingWhat: WritingWhat, categoryId: number,
       pageRole: PageRole, success: (guidelinesSafeHtml: string) => void) {
+  if (!categoryId) {
+    // For now just cancel. There's no draft to load, and there're no guidelines, since
+    // we got no category id.
+    success(null);
+    return;
+  }
   get('/-/load-draft-and-guidelines?writingWhat=' + writingWhat +
       '&categoryId=' + categoryId + '&pageRole=' + pageRole, (response) => {
     success(response.guidelinesSafeHtml);
