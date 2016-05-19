e92223bc669908df19f3c9bdfe2e4870dfae3be3
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2016-05-19 06:36:59 +0200

    Remove old confusing listen port logic.

--- app/debiki/Globals.scala
+++ app/debiki/Globals.scala
@@ -502,11 +502,9 @@ class Globals {
         sys.props.get("testserver.port").map(_.toInt) getOrElse 19001
       }
       else {
-        Play.configuration.getInt("debiki.port") orElse
-          sys.props.get(s"$scheme.port").map(_.toInt) getOrElse {
-            if (Play.isDev) 9000
-            else if (secure) 443
-            else 80
+        Play.configuration.getInt("debiki.port") getOrElse {
+          if (secure) 443
+          else 80
         }
       }
     }
