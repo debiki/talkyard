ea9c6c917ee7a05fc6c7cae2092eff3ee482eaa7
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2018-10-19 08:08:43 +0200

    Add index on all invites, recent first.

--- modules/ty-dao-rdb/src/main/resources/db/migration/y2018/v374__many_invites.sql
+++ modules/ty-dao-rdb/src/main/resources/db/migration/y2018/v374__many_invites.sql
@@ -3,3 +3,5 @@
 drop index dw2_invites_email__u;
 create index invites_emailaddr_invby_i on invites3 (site_id, email_address, created_by_id);
 
+create index invites_invat_i on invites3 (site_id, created_at desc);
+
