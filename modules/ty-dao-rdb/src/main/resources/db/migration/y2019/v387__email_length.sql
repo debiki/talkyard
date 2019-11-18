3457d304dfb41ac8455690870d81c4940bf114aa
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2019-11-18 09:20:41 +0200

    Bump OAuth avatar length to 500
    A Facebook avatar URL, for the test suite, was 263 chars long, so the
    previous 250 chars limit caused an insertion error.

--- modules/ty-dao-rdb/src/main/resources/db/migration/y2019/v387__email_length.sql
+++ modules/ty-dao-rdb/src/main/resources/db/migration/y2019/v387__email_length.sql
@@ -2,3 +2,8 @@ alter table emails_out3 drop constraint dw1_emlot_bodyhtml__c_len;
 alter table emails_out3 add constraint emailsout_c_bodyhtml_len check (
   length(body_html) between 1 and 20000);
 
+-- A Facebook avatar url was 263 chars long, so bump the previous 250 chars limit.
+alter table identities3 drop constraint dw1_ids_avatarurl__c_len;
+alter table identities3 add constraint identities_c_avatarurl_len check (
+  length(avatar_url) between 1 and 500);
+
