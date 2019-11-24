e8f1d99eac7a952064eef7b5c2e6c845c5991772
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2019-11-24 17:07:00 +0100

    itp w, ctd.

--- client/app-slim/model.ts
+++ client/app-slim/model.ts
@@ -1509,7 +1509,35 @@ interface EditPageResponse {
 interface LoginPopupLoginResponse {
   status: 'LoginOk' | 'LoginFailed';
   queryString?: string;
-  currentPageSessionId: string;  // RENAME to embeddedSessionId? [05KSHFJ5]
+  currentPageSessionId: string;  // RENAME to embeddedSessionId? [05KSHFJ5] [NOCOOKIES]
+     // + make these session ids "weaker" than session ids in a top level main win —
+     // because there's a small risk that "foreign" Javascript on the embedding
+     // pages steal the session id (it'd be stored in LocalStorage).
+     /* Document somewhere:
+     ITP 2.0 (June 2018): Apple blocks what it think is third-party tracking cookies immediately.
+     Previously, with ITP 1.0, those cookies were allowed for 24 hours.
+     See: https://impact.com/partnerships/what-does-itp-2-0-mean-for-you-2/
+     ITP 2.2 (Apr 24, 2019): Apple also blocks 1st party cookies that looks like tracking cookies —
+       because advertisers started using 1st party cookies insetad, when 3rd party
+       cookies got that much blocked.
+       https://impact.com/partnerships/safaris-done-it-again-what-you-need-to-know-about-itp-2-2/
+       https://webkit.org/blog/8828/intelligent-tracking-prevention-2-2/
+
+     ITP 2.2 deletes a cookie after one day, if:
+      - Cookie set via js (i.e. document.cookie), and
+      - The site that sent the user to the landing page (via an ad link?)
+        seems to be able to do cross-site-tracking  [how?], and
+      - The link (which link? The ad link?) has a query string or url fragment,
+        indicating some tracking params are sent.
+
+    ITP 2.3: Deletes local storage etc of target website, if via link w query params?:
+    https://www.cognetik.com/blog/new-changes-and-challenges-for-analysts-safari-introduces-itp-2-3/
+
+    Safari 13 on macOS includes ITP Debug Mode
+    """
+    website.example will be marked for non-cookie website data deletion if the user is navigated from a domain classified with cross-site tracking capabilities to a final URL with a query string and/or a fragment identifier, such as website.example?clickID=0123456789
+    """
+     */
 }
 
 interface GuestLoginResponse {   // RENAME to SignupOrGuestOrPasswordLoginResponse?
