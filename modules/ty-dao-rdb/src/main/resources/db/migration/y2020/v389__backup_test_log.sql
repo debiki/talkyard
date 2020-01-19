7b91a1313325d6d548f4419a25c8e3b5a766169f
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2020-01-19 15:47:50 +0100

    Add error_page 502, 503, 504. And a new db migration.

---
+++ modules/ty-dao-rdb/src/main/resources/db/migration/y2020/v389__backup_test_log.sql
@@ -0,0 +1,5 @@
+
+alter table backup_test_log3 add column file_name varchar;
+alter table backup_test_log3 add constraint backuptestlog_c_filename_len check (
+  length(file_name) between 1 and 100);
+
