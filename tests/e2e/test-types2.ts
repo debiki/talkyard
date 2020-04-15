57672f94fe576aad8d446c7bbdc8ab2b72ce4d42
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2020-04-15 14:38:30 +0200

    Fix build: Move types.

--- tests/e2e/test-types2.ts
+++ tests/e2e/test-types2.ts
@@ -1,38 +1,11 @@
 /// <reference path="../../client/app-slim/constants.ts" />
 /// <reference path="../../client/types-and-const-enums.ts" />
 
-// It's unclear if 'browser' refers to an instance of TyE2eTestBrowser
-// or WebdriverIO.BrowserObject, so let's avoid that name.
-declare const wdioBrowser: WebdriverIO.BrowserObject;
-declare const wdioBrowserA: WebdriverIO.BrowserObject;
-declare const wdioBrowserB: WebdriverIO.BrowserObject | U;
-declare const wdioBrowserC: WebdriverIO.BrowserObject | U;
-
-// Rename to  wdioBrowserA  instead:
-declare const browserA: WebdriverIO.BrowserObject;
-declare const browserB: WebdriverIO.BrowserObject | U;
-declare const browserC: WebdriverIO.BrowserObject | U;
-
 
 type LogLevel = 'silent' | 'verbose' | 'command' | 'data' | 'result' | 'error';
 
 type BoolOrFn = boolean | (() => boolean);
 type StringOrFn = string | (() => string);
-
-const enum IsWhere {
-  Forum = 1,
-  LoginPopup = 2,
-
-  EmbFirst = 3,
-  EmbeddingPage = 3,
-  EmbCommentsIframe = 4,
-  EmbEditorIframe = 5,
-  EmbLast = 5,
-
-  // Another server, e.g. Google's OAuth login page. But not an
-  // embedding blog post page.
-  External = 10,
-}
 
 
 interface TestSettings {
