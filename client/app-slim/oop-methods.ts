dcf41a78396d4987c546081f3305218c49506258
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2020-04-20 05:14:24 +0200

    Add "Ask Question" button in forum category view. Tests missing.

--- client/app-slim/oop-methods.ts
+++ client/app-slim/oop-methods.ts
@@ -528,9 +528,9 @@ export function store_isPageDeleted(store: Store): boolean {
 }
 
 
-export function store_mayICreateTopics(store: Store, category: Category): boolean {
+export function store_mayICreateTopics(store: Store, category: Category | U): boolean {
   const settings: SettingsVisibleClientSide = store.settings;
-  if (settings.enableForum === false)
+  if (settings.enableForum === false || !category)
     return false;
 
   let may: boolean;
@@ -686,6 +686,8 @@ function store_mayIEditImpl(store: Store, post: Post, isEditPage: boolean): bool
 }
 
 
+// Also see: store_getCurrOrDefaultCat(store) [GETACTDEFCAT]
+//
 export function store_findTheDefaultCategory(store: Store): Category | undefined {
   return _.find(store.currentCategories, (category: Category) => {
     return category.isDefaultCategory;
