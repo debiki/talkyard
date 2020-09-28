f8e03a6279ca1ab702dff45717cc59abb9bc5064
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2020-09-28 17:40:18 +0200

    Nginx log level: Warn instead of Debug.

--- images/web/run-envsubst.sh
+++ images/web/run-envsubst.sh
@@ -5,6 +5,7 @@
 # Don't forget to add default values in the Dockerfile. [0KW2UY3]
 
 vars='
+  \${TY_NGX_ERR_LOG_LEVEL}
   \${ED_NGX_LIMIT_CONN_PER_IP}
   \${ED_NGX_LIMIT_CONN_PER_SERVER}
   \${ED_NGX_LIMIT_REQ_PER_IP}
@@ -19,7 +20,13 @@ vars='
   \${TY_MAX_AGE_WEEK}
   \${TY_MAX_AGE_DAY}'
 
-envsubst "$vars" < /etc/nginx/nginx.conf.template             > /etc/nginx/nginx.conf
+
+# Or use instead: https://github.com/a8m/envsubst
+# so can place default values in the placeholders instead, not
+# everything here.
+TY_NGX_ERR_LOG_LEVEL="${TY_NGX_ERR_LOG_LEVEL:-warn}" \
+  envsubst "$vars" < /etc/nginx/nginx.conf.template           > /etc/nginx/nginx.conf
+
 envsubst "$vars" < /etc/nginx/http-limits.conf.template       > /etc/nginx/http-limits.conf
 envsubst "$vars" < /etc/nginx/server-limits.conf.template     > /etc/nginx/server-limits.conf
 envsubst "$vars" < /etc/nginx/server-locations.conf.template  > /etc/nginx/server-locations.conf
