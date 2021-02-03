e659176c89dc26fbea43b2fe076b42f325e1186b
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2021-02-03 08:25:44 +0100

    Bump version to v0.2021.06, make bump script work with 0X.

--- s/bump-versions.sh
+++ s/bump-versions.sh
@@ -8,13 +8,25 @@ set -o pipefail  # exit on false | true
 
 old_v=$(cat version.txt)
 
-old_patch_nr=$(  \
+old_nr=$(  \
     echo $old_v | sed -nr 's/^v[0-9]+\.[0-9]+\.([0-9]+).*$/\1/p')
 
-next_patch_nr=`printf '%d' $(($old_patch_nr + 1))`
+# Remove any leading 0 or Bash thinks 08 is base 8  (not base 10).
+# No quotes needed in [[ =~ ]].
+if [[ $old_nr =~ ^0[0-9]$ ]]; then
+  old_nr="$(echo "$old_nr" | sed 's/^0//')"
+fi
+
+next_nr=`printf '%d' $(($old_nr + 1))`
+
+# Pad N to 0N, since the in-year release nr should always be 2 digits, e.g. 07 not 7.
+# No quotes needed in [[ =~ ]].
+if [[ $next_nr =~ ^.$ ]]; then
+  next_nr="0$next_nr"
+fi
 
 next_v=$(echo $old_v  \
-    | sed -nr "s/^(v[0-9]+\.[0-9]+\.)([0-9]+)(.*)\$/\1${next_patch_nr}\3/p" )
+    | sed -nr "s/^(v[0-9]+\.[0-9]+\.)([0-9]+)(.*)\$/\1${next_nr}\3/p" )
 
 echo "$next_v" > version.txt
 
