15cc2fb4f2d86c8a93f37521687ea37a97bb9567
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2021-07-06 15:59:10 +0200

    Generate wdio7 project
    Like so:
      yarn add --dev @wdio/cli
      npx wdio config
    
    I did that in another directory, and copied the resulting files to here.
    Otherwise apparently Yarn wants to reuse the yarn.lock and node_modules
    in the ../../ dir (the main repo base dir).

---
+++ tests/e2e-wdio7/specs/example.e2e.ts
@@ -0,0 +1,15 @@
+import LoginPage from  '../pageobjects/login.page';
+import SecurePage from '../pageobjects/secure.page';
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+});
+
+
