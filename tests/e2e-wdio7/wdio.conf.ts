790cfa9ec3766bf6e79db10f567054ffacc90f14
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2021-07-08 15:15:41 +0200

    Copy default text from wdio 7 auto gen config file.

--- tests/e2e-wdio7/wdio.conf.ts
+++ tests/e2e-wdio7/wdio.conf.ts
@@ -186,14 +186,21 @@ export const config = { // doesn't work: WebdriverIO.Config = {
   // Specify Test Files
   // ==================
   // Define which test specs should run. The pattern is relative to the directory
-  // from which `wdio` was called. Notice that, if you are calling `wdio` from an
-  // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
-  // directory is where your package.json resides, so `wdio` will be called from there.
+  // from which `wdio` was called.
+  //
+  // The specs are defined as an array of spec files (optionally using wildcards
+  // that will be expanded). The test for each spec file will be run in a separate
+  // worker process. In order to have a group of spec files run in the same worker
+  // process simply enclose them in an array within the specs array.
+  //
+  // If you are calling `wdio` from an NPM script (see https://docs.npmjs.com/cli/run-script),
+  // then the current working directory is where your `package.json` resides, so `wdio`
+  // will be called from there.
 
   specs,
 
   exclude: [
-    specsPathPrefix + '/specs/**/*__e2e-test-template__*.ts',
+    specsPathPrefix + './specs/**/*__e2e-test-template__*.ts',
   ],
 
 
@@ -224,6 +231,21 @@ export const config = { // doesn't work: WebdriverIO.Config = {
     // (where 'wildcard' can be anything).
     // See: <../../../docs/wildcard-dot-localhost.md>.
   ],
+  /* Default:
+    capabilities: [{
+
+        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        // grid with only 5 firefox instances available you can make sure that not more than
+        // 5 instances get started at a time.
+        maxInstances: 5,
+
+        browserName: 'chrome',
+        acceptInsecureCerts: true
+        // If outputDir is provided WebdriverIO can capture driver session logs
+        // it is possible to configure which logTypes to include/exclude.
+        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+        // excludeDriverLogs: ['bugreport', 'server'],
+    }], */
 
 
   // ===================
@@ -233,12 +255,13 @@ export const config = { // doesn't work: WebdriverIO.Config = {
 
   // Level of logging verbosity: trace | debug | info | warn | error | silent
   logLevel: settings.logLevel || 'warn',
+
   // Set specific log levels per logger
   // loggers:
   // - webdriver, webdriverio
   // - @wdio/applitools-service, @wdio/browserstack-service, @wdio/devtools-service, @wdio/sauce-service
   // - @wdio/mocha-framework, @wdio/jasmine-framework
-  // - @wdio/local-runner, @wdio/lambda-runner
+  // - @wdio/local-runner
   // - @wdio/sumologic-reporter
   // - @wdio/cli, @wdio/config, @wdio/sync, @wdio/utils
   // Level of logging verbosity: trace | debug | info | warn | error | silent
@@ -271,6 +294,10 @@ export const config = { // doesn't work: WebdriverIO.Config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
+
+  /* Default:
+  services: ['chromedriver','devtools','firefox-profile'], // 'docker'],
+  */
 
   // This makes wdio/something hang, and spin the CPU to 100% "forever",
   // when a login popup tab closes itself (which they do, after login).
@@ -335,7 +362,7 @@ export const config = { // doesn't work: WebdriverIO.Config = {
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
-  // see also: https://webdriver.io/docs/frameworks.html
+  // see also: https://webdriver.io/docs/frameworks
 
   // Make sure you have the wdio adapter package for the specific framework installed
   // before running any tests.
@@ -347,7 +374,7 @@ export const config = { // doesn't work: WebdriverIO.Config = {
   specFileRetries: settings.specFileRetries,
 
   // Whether or not retried specfiles should be retried immediately or deferred to the end of the queue
-  // specFileRetriesDeferred: true, — the default
+  // specFileRetriesDeferred: true,
 
   // Delay in seconds between the spec file retry attempts
   // Set to some seconds, because ofen the problem is that the server is
@@ -359,6 +386,17 @@ export const config = { // doesn't work: WebdriverIO.Config = {
   // see also: https://webdriver.io/docs/dot-reporter.html
   reporters: [TyWdioReporter],
   //  also: 'spec' or 'dot',  or 'concise' — but won't print which file failed :- (
+
+
+  // Options to be passed to Mocha.
+  // See the full list at http://mochajs.org/
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: settings.waitforTimeout,
+    grep: settings.grep,
+    // Bail after first test failure. Saves time, and can inspect the Selenium logs.
+    bail: true,
+  },
 
 
   // =====
@@ -408,9 +446,10 @@ export const config = { // doesn't work: WebdriverIO.Config = {
    * Gets executed before test execution begins. At this point you can access to all global
    * variables like `browser`. It is the perfect place to define custom commands.
    * @param {Array.<Object>} capabilities list of capabilities details
-   * @param {Array.<String>} specs List of spec file paths that are to be run
+   * @param {Array.<String>} specs        List of spec file paths that are to be run
+   * @param {Object}         browser      instance of created browser/device session
    */
-  before: function (capabilities: WebDriver.DesiredCapabilities, specs: string[]) {
+  before: function (capabilities: WebDriver.DesiredCapabilities, specs: string[], browser) {
     // Any way to get the 'cid' here?
 
     // This is in a wdio worker process — it has different 'global.*' than the main process,
@@ -437,7 +476,7 @@ export const config = { // doesn't work: WebdriverIO.Config = {
     // It's nice if browserA is available also in not-multiremote tests with one browser.
     // so there's a way to refer to just *one* browser instead of 
     if (!global.browserA && _.isArray(capabilities) && capabilities.length === 1) {
-      global.browserA = global.browser;
+      global.browserA = global.browser;  // or use the new 'browser' param instead?
     }
     // Let's do this instead: (if the test uses only one browser, then,
     // browserA is undefined, and .browser is the only browser. But, in multiremote,
@@ -623,13 +662,14 @@ export const config = { // doesn't work: WebdriverIO.Config = {
 
 // Options to be passed to Mocha. See the full list at http://mochajs.org.
 // (There's a WebdriverIO MochaOptsConfig interface somewhere, but how import it?)
+/* included above
 (config as any).mochaOpts = {
   ui: 'bdd',
   timeout: settings.waitforTimeout,
   grep: settings.grep,
   // Bail after first test failure. Saves time, and can inspect the Selenium logs.
   bail: true,
-};
+}; */
 
 
 
