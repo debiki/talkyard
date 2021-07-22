cf35a0d07a5388e35e88cb6d7e86b7cf1f3817a3
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2021-07-22 09:08:41 +0200

    Break out code from s/tyd.ts by deleting lines from copied files.

--- s/impl/tyd-util.ts
+++ s/impl/tyd-util.ts
@@ -1,70 +1,15 @@
 import * as _  from 'lodash';
-import * as minimist from 'minimist';
 import * as fs from 'fs';
 import * as glob from 'glob';
 import { die, dieIf, logMessage, logMessageIf, logDebug, logError, logErrorIf, logUnusual
-            } from '../tests/e2e/utils/log-and-die';
+            } from '../../tests/e2e/utils/log-and-die';
 import { ChildProcess, spawn as _spawnAsync, spawnSync as _spawnSync } from 'child_process';
-import { argv } from 'process';
-
-// Bash, Zsh, Fish shell command line completion:
-// ----------------------------------------------
-
-// Omelette intro: https://github.com/f/omelette/issues/33
-
-const omelette = require('omelette');
-const completion = omelette('tyd <mainCmd>')
-
-completion.on('mainCmd', ({ reply }) => {
-  reply([
-        'h', 'help',
-        'u', 'up', //'watchup',
-        'runl', // 'recreate-up-no-limits'  — not impl though. See
-                // docker-compose-no-limits.yml.
-        'w', 'watch',
-        'ps',
-        'k', 'kill', 'ka', 'kw',
-        'r', 'restart', 'ra',
-        'down',
-        'recreate',
-        'rebuild',
-        'l', 'logs', 'lr', 'logsrecent',
-        'lo', 'logsold',
-        'e', 'e2e',
-        'cleane2elogs',
-        'cd', 'clidb',
-        'ca', 'cliapp',
-        'nodejs',
-        'yarn',
-        'gulp',
-        ]);
-});
-
-completion.init();
 
 
-// Nice!: https://github.com/f/omelette  — zero deps :-)
-// or?: https://github.com/mattallty/Caporal.js — no, to many deps.
-// (There's also:  https://github.com/mklabs/tabtab but abandoned?)
-
-// Maybe later: https://github.com/denoland/deno, nice!: function Deno.watchFs
-
-//   this:  s/tyd e2e extaut
-//   runs all end-to-end tests for auth at external IDP (e.g. FB or Gmail)
-//
-// Test traits:
-//  1br, 2br, 3br,  b3c  mtime  extaut extln  odic  embcom  embfor
-//
-// Ex:
-// signup-w-goog.1br.extaut.ts
-// signup-w-linkedin.1br.extaut.ts
-// link-previews-twitter.1br.extln.ts
+export type ExitCode = Nr | Nl;
 
 
-type ExitCode = Nr | Nl;
-
-
-function spawnInBackground(cmdMaybeArgs: St, anyArgs?: St[]): ChildProcess { // , opts: { pipeStdIn?: Bo } = {})
+export function spawnInBackground(cmdMaybeArgs: St, anyArgs?: St[]): ChildProcess { // , opts: { pipeStdIn?: Bo } = {})
   let cmd = cmdMaybeArgs;
   let args = anyArgs;
 
@@ -82,7 +27,7 @@ function spawnInBackground(cmdMaybeArgs: St, anyArgs?: St[]): ChildProcess { // 
 }
 
 
-function spawnInForeground(cmdMaybeArgs: St, anyArgs?: St[], env?: NodeJS.ProcessEnv)
+export function spawnInForeground(cmdMaybeArgs: St, anyArgs?: St[], env?: NodeJS.ProcessEnv)
         : ExitCode {
   let cmd = cmdMaybeArgs;
   let args = anyArgs;
@@ -106,25 +51,7 @@ function spawnInForeground(cmdMaybeArgs: St, anyArgs?: St[], env?: NodeJS.Proces
 
 
 
-// Skip the firts two, argv[1] = /usr/bin/node  argv[2] = <path-to-repo>/s/tyd.
-// bash$ script.ts command cmdB cmdC --opt 1 --optB=2
-// places [command, cmdB, cmdC] in field '_', and opt-vals in key-vals.
-const _tmpCommandsAndOpts: any = minimist(process.argv.slice(2));
-const subCmdsAndOpts = process.argv.slice(3);
-const commands = _tmpCommandsAndOpts['_'];
-const opts = _tmpCommandsAndOpts;
-delete opts._;
- 
-const mainCmd = commands[0];
-dieIf(!mainCmd, `No main command? '${process.argv.join(' ')}' TyE52SKDM7`)
-dieIf(mainCmd !== process.argv[2], `Weird main command: '${mainCmd}' TyE52SKDM5`)
-
-const mainSubCmd: St = commands[1];
-const allSubCmds: St[] = commands.slice(1);
-const allSubCmdsSt: St = allSubCmds.join(' ');
-let mainCmdIsOk: U | true;
-
-function stringifyOpts(opts: { [k: string]: any }): St {
+export function stringifyOpts(opts: { [k: string]: any }): St {
   return _.map(opts, (v, k: St) => {
     // For now, don't allow quotes in opts?
     const vStr = _.isString(v) ? v : '';
@@ -141,314 +68,7 @@ function stringifyOpts(opts: { [k: string]: any }): St {
     return `--${k}${spaceValue}`;
   }).join(' ');
 }
-
-logDebug(`commands: ${commands.join(' ')}`);
-logDebug(`    opts: ${JSON.stringify(opts)}`);
-logDebug(`opts str: ${stringifyOpts(opts)}`);
-
-const yarnOfflineSt = opts.offline || opts.o ? '--offline' : '';
-
-
-if (mainCmd === 'h' || mainCmd === 'help') {
-  logMessage(`You help me or I help you?`);
-  process.exit(0);
-}
-
-
-if (mainCmd === 'nodejs') {
-  spawnInForeground('docker-compose run --rm nodejs ' + subCmdsAndOpts.join(' '));
-  process.exit(0);
-}
-
-
-if (mainCmd === 'yarn') {
-  // Maybe  --no-bin-links?  [x_plat_offl_builds]
-  spawnInForeground('docker-compose run --rm nodejs yarn ' + subCmdsAndOpts.join(' '));
-  process.exit(0);
-}
-
-
-if (mainCmd === 'gulp') {
-  spawnInForeground('docker-compose run --rm nodejs gulp ' + subCmdsAndOpts.join(' '));
-  process.exit(0);
-}
-
-
-if (mainCmd === 'cd' || mainCmd === 'clidb') {
-  spawnInForeground('make db-cli');
-  process.exit(0);
-}
-
-
-if (mainCmd === 'ca' || mainCmd === 'cliapp') {
-  spawnInForeground('make dead');
-  spawnInForeground('s/d-cli');
-  process.exit(0);
-}
-
-
-if (mainCmd === 'ps') {
-  spawnInForeground('docker-compose ps');
-  process.exit(0);
-}
-
-
-if (mainCmd === 'l' || mainCmd === 'logs') {
-  tailLogsThenExit();
-}
-
-function tailLogsThenExit() {
-  spawnInForeground(`docker-compose logs -f --tail 0 ${allSubCmdsSt}`);
-  process.exit(0);
-}
-
-
-if (mainCmd === 'lr' || mainCmd === 'logsrecent') {
-  spawnInForeground(`docker-compose logs -f --tail 555 ${allSubCmdsSt}`);
-  process.exit(0);
-}
-
-
-if (mainCmd === 'lra' || mainCmd === 'logsrecentapp') {
-  spawnInForeground('docker-compose logs -f --tail 555 app');
-  process.exit(0);
-}
-
-
-if (mainCmd === 'lo' || mainCmd === 'logsold') {
-  spawnInForeground(`docker-compose logs ${allSubCmdsSt}`);
-  process.exit(0);
-}
-
-
-if (mainCmd === 'tw' || mainCmd === 'transpilewatch') {
-  spawnInForeground(`make debug_asset_bundles`);
-  spawnInForeground('make watch');
-  process.exit(0);
-}
-
-
-if (mainCmd === 'w' || mainCmd === 'watch') {
-  spawnInForeground('make watch');
-  process.exit(0);
-}
-
-
-if (mainCmd === 'u' || mainCmd === 'up') {
-  mainCmdIsOk = true;
-
-  // If only starting some specific containers, skip Yarn and Make.
-  if (mainSubCmd) {
-    spawnInForeground(`docker-compose up -d ${allSubCmdsSt}`);
-    tailLogsThenExit();
-  }
-
-  // First, update assets bundles once in the foreground — it's annoying
-  // if instead that's done while the server is starting, because then the server
-  // might decide to stop and restart just to pick up any soon newly built bundles?
-  // (Also, log messages from make and the app server get mixed up with each other.)
-  // Maybe  --no-bin-links?  [x_plat_offl_builds]
-  spawnInForeground(`docker-compose run --rm nodejs yarn install ${yarnOfflineSt}`);
-  let exitCode = spawnInForeground('make debug_asset_bundles');
-  if (exitCode >= 1) {
-    logError(`Error building asset bundles, Make exit code: ${exitCode}`)
-    process.exit(exitCode);
-  }
-
-  // Run `up -d` in foreground, so we won't start the `logs -f` process too soon
-  // — that process would exit, if `up -d` hasn't yet started any containers.
-  spawnInForeground('docker-compose up -d');
-
-  // Just this:
-  spawnInForeground('docker-compose logs -f --tail 0');
-  // ... instead of the below,
-
-  // ... Skip this, because 'make watch' and assets rebuild problems
-  // can get hidden by app server log messages.
-  // Better use two different shell terminals, split screen,
-  // one for building assets, another for app & web server logs.
-  // And, slightly complicated with a background process and terminating it later.
-  // Now time to start rebuilding asset bundles in the background, when needed.
-  /*
-  const rebuildAssetsCmd = 'make watch';
-  const watchChildProcess = spawnInBackground(rebuildAssetsCmd);
-
-  const watchExitedPromise = new Promise<ExitCode>(function(resolve, reject) {
-    watchChildProcess.once('exit', function(exitCode: ExitCode) {
-      (makeShouldExit ? logMessage : logError)(
-            `'${rebuildAssetsCmd}' exited, code: ${exitCode}`);
-      resolve(exitCode);
-    });
-  })
-
-  let makeShouldExit = false;
-
-  // Don't exit immetiately on CTRL+C — first, stop  make watch.
-  // But!  'make watch' uses inotifywait, which con't stop :-(
-  // Maybe switch to https://github.com/paulmillr/chokidar  instead?
-  // And watch client/  and app/  and ty-dao-rdb  and ty-core, call Make
-  // on any change?
-  process.on('SIGINT', function() {
-    logMessage(`Caught SIGINT.`);
-    // We'll continue after  spawnInForeground() below. (Need do nothing here.)
-  });
-
-  // Show logs until CTRL+C.
-  // (There's also:  process.on('SIGINT', function() { ... });
-  spawnInForeground('docker-compose logs -f --tail 0');
-
-  logMessage(`Stopping '${rebuildAssetsCmd}' ...`);
-  makeShouldExit = true;
-  watchChildProcess.kill();
-
-  setTimeout(function() {
-    logError(`'${rebuildAssetsCmd}' takes long to exit, I'm quitting anyway, bye.`);
-    process.exit(0);
-  }, 9000);
-
-  watchExitedPromise.finally(function() {
-    logMessage(`Bye. Server maybe still running.`);
-    process.exit(0);
-  });
-  */
-}
-
-
-if (mainCmd === 'ra') {
-  restartContainers('app');
-}
-if (mainCmd === 'r' || mainCmd === 'restart') {
-  restartContainers(allSubCmdsSt); // e.g. 'web app'
-}
-
-function restartContainers(containers: St) {
-  const cs = (containers || '').trim();
-  logMessage(cs ? `Stopping containers: ${cs}...` : `Stopping all containers...`);
-  spawnInForeground('sh', ['-c', `s/d kill ${cs}`]);
-
-  // If restarting the web/app, probably we want up-to-date assets?
-  if (!containers || containers.includes('web') || containers.includes('app')) {
-    logMessage(`Rebuilding assets if needed ...`);
-    spawnInForeground('make debug_asset_bundles');
-  }
-
-  logMessage(`Starting containers...`);
-  spawnInForeground('sh', ['-c', `s/d start ${cs}`]);
-  tailLogsThenExit();
-}
-
-
-if (mainCmd === 'recreate') {
-  const cs = allSubCmdsSt;  // which containers, e.g.  'web app'
-  spawnInForeground('sh', ['-c', `s/d kill ${cs}; s/d rm -f ${cs}; s/d up -d ${cs}`]);
-  tailLogsThenExit();
-}
-
-
-if (mainCmd === 'rebuild') {
-  rebuild();
-}
-
-function rebuild() {
-  const cs = allSubCmdsSt;  // which containers, e.g.  'web app'
-  logMessage(`\n**Removing: ${cs} **`)
-  spawnInForeground('sh', ['-c', `s/d kill ${cs}; s/d rm -f ${cs}`]);
-  logMessage(`\n**Building: ${cs} **`)
-  spawnInForeground('sh', ['-c', `s/d build ${cs}`]);
-  logMessage(`\n**Done rebuilding: ${cs}. Bye.**`)
-  process.exit(0)
-}
-
-
-if (mainCmd === 'rs' || mainCmd === 'rebuildrestart') {
-  const cs = allSubCmdsSt;  // which containers
-  rebuild();
-  logMessage(`\n**Starting: ${cs} **`)
-  spawnInForeground('sh', ['-c', `s/d up -d ${cs}`]);
-  tailLogsThenExit();
-}
-
-
-if (mainCmd === 'ka' || (mainCmd === 'kill' && mainSubCmd == 'app')) {
-  spawnInForeground('s/d kill app');
-  process.exit(0);
-}
-
-
-if (mainCmd === 'kw' || (mainCmd === 'kill' && mainSubCmd == 'web')) {
-  spawnInForeground('s/d kill web');
-  process.exit(0);
-}
-
-
-if (mainCmd === 'k' || mainCmd === 'kill') {
-  killAllContainers();
-  process.exit(0);
-}
-
-function killAllContainers() {
-  spawnInForeground('make dead');
-}
-
-
-if (mainCmd === 'down') {
-  killAllContainers();
-  spawnInForeground('s/d down');
-  process.exit(0);
-}
-
-
-if (mainCmd === 'cleane2elogs') {
-  spawnInForeground('rm -fr target/e2e-test-logs');
-  spawnInForeground('mkdir target/e2e-test-logs');
-  process.exit(1);
-}
-
-
-// -----------------------------------------------------------------------
-//  E2E and API tests
-// -----------------------------------------------------------------------
-
-const useHttps = argv.includes('--secure') || argv.includes('--https');
-
-if (useHttps) {
-  logMessage(`Will use HTTPS, because --secure flag. Disabling HTTPS certificate checks`);
-  process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = '0';
-}
-
-
-// -----------------------------------------------------------------------
-//  API and Typescript/Reactjs unit tests
-// -----------------------------------------------------------------------
-
-// We use Jest, but Jest doesn't like custom command line flags — it says
-// 'Unrecognized option "the_option_name"' and exits.
-// Instead, we'll pass Ty command line options to Ty's test suite code,
-// via an env var — this, Jest won't notice.
-
-
-if (mainCmd === 'tapi' || mainCmd === 'testapi') {
-  const jestTestEnv: NodeJS.ProcessEnv = {
-    // slice(2) drops 's/tyd testapi'.
-    'TY_ENV_ARGV_ST': process.argv.slice(2).join(' '),  // use json instead
-  };
-
-  // (Another approach could be sth like:
-  // process.env.__CONFIG = JSON.stringify(...)
-  // require('jest-cli/bin/jest');
-  // process.argv = ['node', 'jest', '--config', 'tests/api/jest.config.ts']
-  // — but won't work, if we switch to Deno instead, for this s/tyd.ts script?)
-
-  spawnInForeground('./node_modules/.bin/jest',
-        ['--config', 'tests/api/jest.config.ts'], jestTestEnv);
-
-  process.exit(1);
-}
-
-// -----------------------------------------------------------------------
-// -----------------------------------------------------------------------
-
-
+/*
 
 if (mainCmd === 'e' || mainCmd === 'e2e') {
 
@@ -633,33 +253,52 @@ async function runE2eTests(): Promise<ExitCode> {
 
   // TODO   Don't run magic time tests in parallel — they mess up the
   // time for each other.
+  */
 
-  const serverAndDirByPort: { [portNr: string]: [ChildProcess, St] } = {};
+interface StaticFileServer {
+  serverProcess: ChildProcess;
+  relativeFileDir: St;
+  numWantsIt: Nr;
+}
 
-  function startStaticFileServer(portNr: Nr, relDir: St) {
-    const anyOld = serverAndDirByPort[portNr];
-    if (anyOld) {
-      const oldDir = anyOld[1];
-      if (oldDir === relDir) {
-        // Already started.
-        return;
-      }
-      const oldServer: ChildProcess = anyOld[0];
-      oldServer.kill();
-    }
-    const cp = spawnInBackground(`node_modules/.bin/http-server -p${portNr} ${relDir}`);
-    serverAndDirByPort[portNr] = [cp, relDir];
+const serverAndDirByPort: { [portNr: string]: StaticFileServer } = {};
+
+export function startStaticFileServer(portNr: Nr, relativeFileDir: St) {
+  const server = serverAndDirByPort[portNr];
+  if (!server) {
+    const serverProcess: ChildProcess = spawnInBackground(
+            `node_modules/.bin/http-server -p${portNr} ${relativeFileDir}`);
+    serverAndDirByPort[portNr] = {
+      serverProcess,
+      relativeFileDir,
+      numWantsIt: 1,
+    };
   }
-
-  function stopStaticFileServer(portNr: Nr) {
-    const anyOld = serverAndDirByPort[portNr];
-    if (anyOld) {
-      const oldServer: ChildProcess = anyOld[0];
-      oldServer.kill();
-    }
+  else {
+    dieIf(server.relativeFileDir !== relativeFileDir,
+          `Trying to starts many servers on the same port, but different file dirs ` +
+          `[TyE60RMD25]`);
+    server.numWantsIt = server.numWantsIt + 1;
   }
+}
 
 
+export function stopStaticFileServer(ps: { portNr: Nr, stopAnyAndAll?: Bo }) {
+  const server = serverAndDirByPort[ps.portNr];
+  if (!server && ps.stopAnyAndAll)
+    return;
+
+  dieIf(!server, `No server to stop at port ${ps.portNr} [TyE60RMD27]`);
+  server.numWantsIt = server.numWantsIt - 1;
+  dieIf(server.numWantsIt < 0, `Less than 0 wants server [TyE60RMD28]`);
+  if (server.numWantsIt === 0 || ps.stopAnyAndAll) {
+    const childProcess: ChildProcess = server.serverProcess;
+    childProcess.kill();
+    delete serverAndDirByPort[ps.portNr];
+  }
+}
+
+/*
   const skipAlways = ['!UNIMPL', '!-impl.', '!imp-exp-imp-exp-site'];
   const skipEmbAndAlways = ['!embcom', '!embedded-', '!embforum.', ...skipAlways]
   const skip2And3Browsers = ['!.2br', '!.3br'];
@@ -807,3 +446,4 @@ else if (!mainCmdIsOk) {
   console.error(`Werid main command: ${mainCmd}. Error. Bye.  [TyE30598256]`);
   process.exit(1);
 }
+*/
