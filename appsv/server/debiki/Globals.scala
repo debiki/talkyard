d50cd0ca030a0bba763d9635fd9abaa0e50f4206
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2021-12-23 09:40:48 +0100

    Bump renderer again.

--- appsv/server/debiki/Globals.scala
+++ appsv/server/debiki/Globals.scala
@@ -248,7 +248,7 @@ class Globals(  // RENAME to TyApp? or AppContext? TyAppContext? variable name =
   }
 
   // Could rename to "rendererVersion".
-  val applicationVersion = "0.00.78"  // later, read from some build config file
+  val applicationVersion = "0.00.79"  // later, read from some build config file
 
   def applicationSecret: String = _appSecret
 
