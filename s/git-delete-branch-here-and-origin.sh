8fcf2e96dd1c8c1e0dca06088ff28f1a2d866f27
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2022-04-05 10:18:40 +0200

    Fix e2e test.

--- s/git-delete-branch-here-and-origin.sh
+++ s/git-delete-branch-here-and-origin.sh
@@ -12,3 +12,8 @@ git push origin --delete "$branch_name"
 # Delete many with the same name part:
 #  for b in $(git branch -a | grep dependabot | sed s+remotes/origin/++ )  ;  do echo git push origin --delete $b  ;  done
 
+# Delete all from a specific repository, say, tytest, locally only:  (flags  -d -r  deletes the remote branch, locally only)
+#
+#   for b in $(git branch -a  | grep tytest | sed 's#remotes/##') ; do  git branch -d -r $b  ; done
+
+
