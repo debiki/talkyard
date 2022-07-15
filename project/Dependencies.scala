6b879671255caea97d9164584063596f7f9e7d11
Scala Steward <scala_steward@virtuslab.com>
2022-07-15 13:54:40 +0000

    Update java-jwt to 4.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -122,7 +122,7 @@ object Dependencies {
     //
     // Let's use Java-JWT. It's well-known and its readme has a simple decoding example.
     // Repo: https://github.com/auth0/java-jwt
-    val auth0JavaJwt = "com.auth0" % "java-jwt" % "3.19.2"
+    val auth0JavaJwt = "com.auth0" % "java-jwt" % "4.0.0"
 
 
     // ----- PASETO tokens
