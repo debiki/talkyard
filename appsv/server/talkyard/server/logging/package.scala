ed17770cf74344b0683b30c26c64163c21c3f72f
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2023-12-11 16:36:49 +0100

    Copy server package.scala to logging/.

---
+++ appsv/server/talkyard/server/logging/package.scala
@@ -0,0 +1,238 @@
+/**
+ * Copyright (c) 2016, 2021 Kaj Magnus Lindberg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package talkyard
+
+import com.debiki.core._
+import Prelude.{castToInt32, IfBadDie}
+import debiki.Globals
+import play.api.libs.json._
+import play.api.{MarkerContext => p_MarkerContext}
+import java.util.concurrent.atomic.{AtomicInteger => j_AtomicInteger}
+
+
+trait NumSince {
+  def lastAtUnixMinute(): Opt[i32]
+  def numSinceStart(): i32
+}
+
+private class NumSinceImpl extends NumSince {
+  def lastAtUnixMinute(): Opt[i32] = {
+    val atMin = _lastAtUnixMinute.get()
+    if (atMin == 0) None
+    else Some(atMin)
+  }
+
+  def numSinceStart(): i32 = _numSinceStart.get()
+
+  val _lastAtUnixMinute = new j_AtomicInteger(0)
+  val _numSinceStart = new j_AtomicInteger(0)
+}
+
+
+package object server {
+
+  // Hmm, COULD make this part of the server state [use_state] (i.e. Globals
+  // although those are no longer globals, just an object instance).
+  // But then how would erors about creating the server state get logged? — Could
+  // use Play's/Java's log fns directly, when starting the server, until state created.
+  private val _numErrors = new NumSinceImpl()
+  private val _numWarnings = new NumSinceImpl()
+
+  def numErrors: NumSince = _numErrors
+  def numWarnings: NumSince = _numWarnings
+
+  type JsonConf = parser.JsonConf
+  val JsonConf: parser.JsonConf.type = parser.JsonConf
+
+  type p_Result = play.api.mvc.Result
+  val p_Results: play.api.mvc.Results.type = play.api.mvc.Results
+  val p_Status: play.api.http.Status.type = play.api.http.Status
+
+  type p_Logger = play.api.Logger
+
+  type j_NoRouteToHostException = java.net.NoRouteToHostException
+  type j_IOException = java.io.IOException
+  type j_ExecutionException = java.util.concurrent.ExecutionException
+
+
+  val ProdConfFilePath = "/opt/talkyard/conf/play-framework.conf"
+
+  def isDevOrTest: Boolean = Globals.isDevOrTest
+
+  // "tysvapp":  "ty" = Talkyard, "sv" = server, "app" = application.
+  // (Later, more logging?:  tysvweb = web server logs,
+  // tybrapp = browser app logs, tyanapp = android app, tyioapp = iOS app logs)
+  //
+  def TyLogger(name: St, anySiteId: Opt[SiteId] = None): play.api.Logger = {
+    val slf4jLogger = org.slf4j.LoggerFactory.getLogger("tysvapp." + name)
+    new CountingLogger(slf4jLogger)
+  }
+
+  private class CountingLogger(logger: org.slf4j.Logger) extends play.api.Logger(logger) {
+    override def warn(msg: => St)(implicit mc: p_MarkerContext): U = {
+      if (isWarnEnabled) {
+        increment(_numWarnings)
+        super.warn(msg)(mc)
+      }
+    }
+
+    override def warn(msg: => St, err: => Throwable)(implicit mc: p_MarkerContext): U = {
+      if (isWarnEnabled) {
+        increment(_numWarnings)
+        super.warn(msg, err)(mc)
+      }
+    }
+
+    override def error(msg: => St)(implicit mc: p_MarkerContext): U = {
+      if (isErrorEnabled) {
+        increment(_numErrors)
+        super.error(msg)(mc)
+      }
+    }
+
+    override def error(msg: => St, err: => Throwable)(implicit mc: p_MarkerContext): U = {
+      if (isErrorEnabled) {
+        increment(_numErrors)
+        super.error(msg, err)(mc)
+      }
+    }
+  }
+
+  private def increment(numSince: NumSinceImpl): U = {
+    numSince._numSinceStart.incrementAndGet()
+    COULD // use Globals.now() instead  [use_state]
+    // Dupl code [now_mins].
+    val nowMillis = System.currentTimeMillis()
+    val nowMins_i64 = nowMillis / MillisPerMinute
+    val nowMins = castToInt32(nowMins_i64, IfBadDie)
+    // A race, fine, doesn't need to be exact.
+    numSince._lastAtUnixMinute.set(nowMins)
+  }
+
+
+  trait TyLogging {
+    protected val logger: play.api.Logger = newLogger(getClass)
+
+    protected def anySiteId: Opt[SiteId] = None
+
+    protected def anySiteIdPrefix: St = {
+      val id = anySiteId getOrElse {
+        return ""
+      }
+      s"${id}: "
+    }
+
+    protected def bugWarnIf(condition: Boolean, errorCode: String,
+          problem: => String = ""): Boolean = {
+      bugWarnDieIfThen(condition, errorCode, problem, thenDo = null)
+    }
+
+    protected def bugWarnDieIfThen(condition: Boolean, errorCode: String,
+          problem: => String = "", thenDo: () => Unit): Boolean = {
+      if (!condition)
+        return false
+      bugWarn(errorCode, problem)
+      if (thenDo ne null) {
+        thenDo()
+      }
+      true
+    }
+
+    protected def bugWarn(errorCode: String, problem: => String = "") {
+      Prelude.dieIf(Globals.isDevOrTest, errorCode, problem)
+      val message = Prelude.formatErrorMessage(errorCode, problem)
+      logger.warn(anySiteIdPrefix + s"BUG: $message")
+    }
+
+
+    implicit class GetOrBugWarn[V](val underlying: Option[V]) {
+      def getOrBugWarn(errorCode: String, message: => String = "")(block: V => Unit): Unit =
+        underlying match {
+          case None =>
+            bugWarn(errorCode, message)
+          case Some(value: V) =>
+            block(value)
+        }
+    }
+  }
+
+
+  def newLogger(clazz: Class[_], anySiteId: Opt[SiteId] = None): play.api.Logger =
+    TyLogger(clazz.getName.stripSuffix("$"))
+
+
+  implicit class RichResult(val underlying: play.api.mvc.Result) {
+    def statusCode: Int = underlying.header.status
+
+    def bodyAsUtf8String: String = {
+      import play.api.http.HttpEntity
+      underlying.body match {
+        case HttpEntity.Strict(byteString, _) =>
+          byteString.utf8String
+        case _: HttpEntity.Chunked =>
+          "(chunked response)"
+        case _: HttpEntity.Streamed =>
+          "(streamed response)"
+      }
+    }
+  }
+
+
+  val Whatever = "*"
+
+  val UploadsUrlBasePath = "/-/u/"
+  val CustomAssetsUrlBasePath = "/-/site/"
+
+  {
+    val uploadsUrlPath: St = controllers.routes.UploadsController.servePublicFile("").url
+    assert(uploadsUrlPath == UploadsUrlBasePath, "TyEBADUPLPATH9564")
+  }
+
+
+  /** @param html Html for the whole page.
+    * @param unapprovedPostAuthorIds Ids of authors who have posted stuff that hasn't yet been
+    *   approved. If one of these authors views the page, hens unapproved posts should
+    *   be loaded too, so hen can edit them. (Normally, unapproved posts aren't loaded.)
+    */
+  case class RenderedPage(
+    html: String,
+    reactStoreJsonString: String,
+    unapprovedPostAuthorIds: Set[UserId],
+    anonsByRealId: Map[PatId, Seq[Anonym]])
+
+
+  REMOVE // ?
+  implicit object WhenFormat extends Format[When] {
+    def reads(json: JsValue): JsResult[When] = JsSuccess(When.fromMillis(json.as[Long]))
+    def writes(when: When): JsValue = JsNumber(when.millis)
+  }
+
+
+  REMOVE // ?
+  implicit object OptWhenFormat extends Format[Option[When]] {
+    def reads(json: JsValue): JsResult[Option[When]] =
+      if (json == JsNull) JsSuccess(None)
+      else JsSuccess(Some(When.fromMillis(json.as[Long])))
+
+    def writes(when: Option[When]): JsValue = when match {
+      case None => JsNull
+      case Some(w) => JsNumber(w.millis)
+    }
+  }
+
+}
