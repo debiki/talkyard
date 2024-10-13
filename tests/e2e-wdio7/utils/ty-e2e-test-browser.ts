584cc3ca01a9d7ff5b795b03506219a5058a3b02
Kaj Magnus Lindberg <kajmagnus3@gmail.com>
2024-10-13 18:00:15 +0200

    Fix e2e bug: Don't scroll up to the topbar; it's visible already.

--- tests/e2e-wdio7/utils/ty-e2e-test-browser.ts
+++ tests/e2e-wdio7/utils/ty-e2e-test-browser.ts
@@ -1474,14 +1474,23 @@ export class TyE2eTestBrowser {
 
     async scrollIntoViewInPageColumn(selector: St) {   // RENAME to  scrollIntoView
       dieIf(!selector, '!selector [TyE05RKCD5]');
-      const isInPageColResult = await this.#br.execute(function(selector) {
+
+      const where: 'Topbar' | 'PageCol' | 'Elsewhere' | 'Nowhere' =
+              await this.#br.execute(function(sel) {
+        var elem = document.querySelector(sel);
+        if (!elem) return 'Nowhere';
         var pageColumn = document.getElementById('esPageColumn');
-        if (!pageColumn)
-          return false;
-        var elem = document.querySelector(selector);
-        return pageColumn.contains(elem);
+        if (!pageColumn || !pageColumn.contains(elem)) return 'Elsewhere';
+        // The topbar isn't really part of the page column — it's fixed at the top.
+        // But technically, it's a descendant, so need to ignore elems therein.
+        var topbarElm = (document.getElementsByClassName('s_Tb-Fxd') || [])[0];
+        return topbarElm && topbarElm.contains(elem) ? 'Topbar': 'PageCol';
       }, selector);
-      if (isInPageColResult) {
+
+      if (where === 'Topbar') {
+        // Noop. Should already be visible
+      }
+      else if (where === 'PageCol') {
         await this._real_scrollIntoViewInPageColumn(selector);
       }
       else {
